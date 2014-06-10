b9c626cb3625e0f7a585b935bde19db55e6e6518
Nasser Brake <nasser.brake@kroschke.de>
2014-06-10 16:40:01 +0200

    Regel für tf get geändert, alle Dateien werden geprüft.  In Zusammenspiel mit der core.whitespace Konfiguration sehe ich jetzt keine Zeilenenden Probleme beim Vergleichen mehr

--- tf_get.cmd
+++ tf_get.cmd
@@ -1,73 +1,73 @@
 cd inetpub\wwwroot\
 cd servicesmvc
-tf get $/Migration/CKAG2/ServicesMvc /recursive /force
+tf get $/Migration/CKAG2/ServicesMvc /recursive /all
 
 cd ..
 cd AutohausPortal
-tf get $/Migration/AutohausPortal /recursive /force
+tf get $/Migration/AutohausPortal /recursive /all
 
 cd ..
 cd AutohausPortalMvc
-tf get $/Migration/AutohausPortalMvc /recursive /force
+tf get $/Migration/AutohausPortalMvc /recursive /all
 
 cd..
 cd BaseService
-tf get $/Migration/BaseService /recursive /force
+tf get $/Migration/BaseService /recursive /all
 
 cd ..
 cd CarDocuAgis
-tf get $/Migration/WebServices/CarDocuAgis /recursive /force
+tf get $/Migration/WebServices/CarDocuAgis /recursive /all
 
 cd ..
 cd Kantine
-tf get $/Migration/Kantine /recursive /force
+tf get $/Migration/Kantine /recursive /all
 
 cd..
 cd KBS
-tf get $/Migration/CKAG2/KBS /recursive /force
+tf get $/Migration/CKAG2/KBS /recursive /all
 
 cd.. 
 cd nPa
-tf get $/Migration/nPA /recursive /force
+tf get $/Migration/nPA /recursive /all
 
 cd..
 cd nPaService
-tf get $/Migration/nPaService /recursive /force
+tf get $/Migration/nPaService /recursive /all
 
 cd..
 cd Portal
-tf get $/Migration/CKAG/portal /recursive /force
+tf get $/Migration/CKAG/portal /recursive /all
 
 cd..
 cd PortalORM
-tf get $/Migration/PortalORM /recursive /force
+tf get $/Migration/PortalORM /recursive /all
 
 cd..
 cd PortalZLD
 
 cd..
 cd Services
-tf get $/Migration/CKAG2/Services /recursive /force
+tf get $/Migration/CKAG2/Services /recursive /all
 
 cd..
 cd ServicesMvc
-tf get $/Migration/CKAG2/ServicesMvc /recursive /force
+tf get $/Migration/CKAG2/ServicesMvc /recursive /all
 
 cd..
 cd SixtService
-tf get $/Migration/SixtService /recursive /force
+tf get $/Migration/SixtService /recursive /all
 
 cd..
 cd SixtServiceLeas
-tf get $/Migration/SixtServiceLeas /recursive /force
+tf get $/Migration/SixtServiceLeas /recursive /all
 
 cd..
 cd SoapRuecklaeuferschnittstelle
-tf get $/Migration/WebServices/SoapRuecklaeuferschnittstelle /recursive /force
+tf get $/Migration/WebServices/SoapRuecklaeuferschnittstelle /recursive /all
 
 cd..
 cd SoapTester
-tf get $/Migration/WebServices/SoapTester /recursive /force
+tf get $/Migration/WebServices/SoapTester /recursive /all
 cd..
 
 cd StatusDefault
@@ -75,81 +75,81 @@ cd StatusDefault
 
 cd..
 cd StatusmonitorDAD
-tf get $/Migration/DAD-Statusmonitor /recursive /force
+tf get $/Migration/DAD-Statusmonitor /recursive /all
 
 cd..
 cd Validation
-tf get $/Migration/Validation/Validation /recursive /force
+tf get $/Migration/Validation/Validation /recursive /all
 
 cd..
 cd ZLDMobile
-tf get $/Migration/ZLDMobile /recursive /force
+tf get $/Migration/ZLDMobile /recursive /all
 
 cd ..\..\..\
 cd work
 
 cd AutoAct
-tf get $/Migration/Work/AutoAct /recursive /force
+tf get $/Migration/Work/AutoAct /recursive /all
 
 cd..
 cd Base
-tf get $/Migration/CKAG/Base /recursive /force
+tf get $/Migration/CKAG/Base /recursive /all
 
 cd..
 cd BaseORM
-tf get $/Migration/Work/BaseORM /recursive /force
+tf get $/Migration/Work/BaseORM /recursive /all
 
 cd..
 cd BtBase
-tf get $/Migration/Work/BtBase /recursive /force
+tf get $/Migration/Work/BtBase /recursive /all
 
 cd..
 cd CarDocu
-tf get $/Migration/Work/CarDocu /recursive /force
+tf get $/Migration/Work/CarDocu /recursive /all
 
 cd..
 cd CKG
 
 cd..
 cd CkgDomainLogic
-tf get $/Migration/Work/CkgDomainLogic /recursive /force
+tf get $/Migration/Work/CkgDomainLogic /recursive /all
 
 cd..
 cd ErpBase
-tf get $/Migration/Work/ErpBase /recursive /force
+tf get $/Migration/Work/ErpBase /recursive /all
 
 cd..
 cd GAC
-tf get $/Migration/Work/GAC /recursive /force
+tf get $/Migration/Work/GAC /recursive /all
 
 cd..
 cd Ioc
-tf get $/Migration/Work/Ioc /recursive /force
+tf get $/Migration/Work/Ioc /recursive /all
 
 cd..
 cd Log
-tf get $/Migration/Work/Log /recursive /force
+tf get $/Migration/Work/Log /recursive /all
 
 cd..
 cd mobile.de
-tf get $/Migration/Work/mobile.de /recursive /force
+tf get $/Migration/Work/mobile.de /recursive /all
 
 cd..
 cd packages
-tf get $/Migration/Work/packages /recursive /force
+tf get $/Migration/Work/packages /recursive /all
 
 cd..
 cd SapORM
-tf get $/Migration/Work/SapORM /recursive /force
+tf get $/Migration/Work/SapORM /recursive /all
 
 cd..
 cd Tools
-tf get $/Migration/Work/Tools /recursive /force
+tf get $/Migration/Work/Tools /recursive /all
 
 cd..
 cd ZLDBase
-tf get $/Migration/Work/ZLDBase /recursive /force
+tf get $/Migration/Work/ZLDBase /recursive /all
 
 cd..
 cd ZLDBaseMvc 
-tf get $/Migration/Work/ZLDBaseMvc /recursive /force
+tf get $/Migration/Work/ZLDBaseMvc /recursive /all
