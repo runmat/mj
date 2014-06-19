e5d46dab79b65553580272f0966cce5c20ead3b4
Matthias Jenzen <matthias.jenzen@kroschke.de>
2014-06-19 10:58:31 +0200

    Terminüberischt eines Events + Orts

--- Work/CkgDomainLogic/CkgDomainInsurance/Controllers/EventVerwaltung/InsuranceController.cs
+++ Work/CkgDomainLogic/CkgDomainInsurance/Controllers/EventVerwaltung/InsuranceController.cs
@@ -57,7 +57,7 @@ namespace ServicesMvc.Controllers
         }
 
         [CkgApplication]
-        public ActionResult TermineOrtBoxen()
+        public ActionResult TerminUebersicht()
         {
             EventsViewModel.DataInit();
 
