a145c38211e07d0ddf627fedac2626771bfa0b77
Matthias Jenzen <matthias.jenzen@kroschke.de>
2014-07-03 12:55:10 +0200

    added db column "AppIsMvcDefaultFavorite"

--- inetpub/wwwroot/Services/Admin/CustomerManagement.aspx.vb
+++ inetpub/wwwroot/Services/Admin/CustomerManagement.aspx.vb
@@ -402,6 +402,10 @@ Partial Public Class CustomerManagement
             newRow("AppTechType") = row("AppTechType")
             newRow("AppDescription") = row("AppDescription")
             newRow("Assigned") = ""
+            Try
+                newRow("AppIsMvcDefaultFavorite") = False
+            Catch
+            End Try
             tblApps.Rows.Add(newRow)
         Next
 
@@ -418,7 +422,10 @@ Partial Public Class CustomerManagement
             newRow("AppTechType") = row("AppTechType")
             newRow("AppDescription") = row("AppDescription")
             newRow("Assigned") = "X"
-            newRow("AppIsMvcDefaultFavorite") = row("AppIsMvcDefaultFavorite")
+            Try
+                newRow("AppIsMvcDefaultFavorite") = row("AppIsMvcDefaultFavorite")
+            Catch
+            End Try
             tblApps.Rows.Add(newRow)
         Next
 
