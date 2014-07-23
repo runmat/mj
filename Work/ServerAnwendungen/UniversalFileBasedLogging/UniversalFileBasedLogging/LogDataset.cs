4164e049fbcc13b992166c148c58c437282314a7
Nasser Brake <nasser.brake@kroschke.de>
2014-07-23 15:41:41 +0200

    TFS Bindings entfernt.  Projekt UniversalFileBasedLogging in Git aufgenommen.

---
+++ Work/ServerAnwendungen/UniversalFileBasedLogging/UniversalFileBasedLogging/LogDataset.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalFileBasedLogging
+{
+    /// <summary>
+    /// Eine Sammlung von Logeinträgen
+    /// </summary>
+    public class LogDataset : List<LogCustomer>
+    {
+        private static readonly LogDataset LdSearch = new LogDataset();
+        protected static LogCustomer LcSearch;
+        protected static LogFile LfSearch;
+        
+        // Fügt der Liste neue Einträge hinzu, wenn sie nicht bereits existieren
+        public void AddNoDoubles(LogCustomer item)
+        {
+            LcSearch = item;
+            LogCustomer lcFound = this.Find(isCustomer);
+            LcSearch = null;
+
+            if (lcFound != null)
+            {
+                // Dateien des Kunden übertragen
+                lcFound.AddRange(item);
+                return;
+            }
+
+            this.Add(item);
+        }
+
+        public LogCustomer FindCustomer(string name, string archivname)
+        {
+            LcSearch = new LogCustomer(name, archivname, LdSearch);
+            LogCustomer lcFound = this.Find(isCustomer);
+            LdSearch.Clear();
+            return lcFound;
+            
+        }
+
+        // Prüft ob Kunde 2 Kunden gleich sind
+        private static bool isCustomer(LogCustomer custom)
+        {
+            if (LcSearch.Name == custom.Name && LcSearch.Archivname == custom.Archivname)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // liefert eine Datei mit den angegebenen Parametern
+        public LogFile FindFile(string fin, string kennzeichen, string titel)
+        {
+            LfSearch = new LogFile(fin, kennzeichen, titel, new LogCustomer("", "", LdSearch));
+            foreach (LogCustomer custom in this)
+            {
+                return custom.Find(isFile);
+            }
+            return null;
+        }
+
+        private static bool isFile(LogFile file)
+        {
+            bool found = false;
+
+            if (LfSearch.Kennzeichen != null)
+            {
+                if (file.Kennzeichen == LfSearch.Kennzeichen)
+                {
+                    found = true;
+                }
+                else { return false; }
+            }
+
+            if (LfSearch.FIN != null)
+            {
+                if (file.FIN == LfSearch.FIN)
+                {
+                    found = true;
+                }
+                else { return false; }
+            }
+
+            if (LfSearch.Titel != null)
+            {
+                if (file.Titel == LfSearch.Titel)
+                {
+                    found = true;
+                }
+                else { return false; }
+            }
+
+            return found;
+        }
+    }
+}
---
+++ Work/ServerAnwendungen/UniversalFileBasedLogging/UniversalFileBasedLogging/LogFile.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalFileBasedLogging
+{
+    /// <summary>
+    /// Stellt eine Datei dar für die Einträge geloggt werden.
+    /// </summary>
+    public class LogFile : List<LogEntry>
+    {
+        public LogFile(string kennzeichen, string fin, string titel, LogCustomer customer)
+        {
+            Kennzeichen = kennzeichen;
+            FIN = fin;
+            Customer = customer;
+
+            Trys = 0;
+            Filename = "";
+            NameEasy = "";
+            strTyp = "";
+            Finished = false;
+            Titel = titel;
+
+            customer.Add(this);
+        }
+        /// <summary>
+        /// Dateiname
+        /// </summary>
+        public string Filename
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Name der Datei im EasyArchiv
+        /// </summary>
+        public string NameEasy
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Übergeordneter Kunde
+        /// </summary>
+        public LogCustomer Customer
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Versuchsnummer
+        /// </summary>
+        public int Trys
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Kennzeichen
+        /// </summary>
+        public string Kennzeichen
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Fahrgestellnummer
+        /// </summary>
+        public string FIN
+        {
+            get;
+            set;
+        }
+		
+        /// <summary>
+        /// strTyp der im Dateinamen mit verschlüsselt wird
+        /// </summary>
+        public string strTyp
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Datei übertragung abgeschlossen
+        /// </summary>
+        /// <remarks>Wenn Flag gesetzt kann dieser Eintrag beim Parsen übersprungen werden</remarks>
+        public bool Finished
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Titel des Files z.B. COC,ZB2,ZB1,...
+        /// </summary>
+        public string Titel
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() {
+            return Filename;
+        }
+
+    }
+}
