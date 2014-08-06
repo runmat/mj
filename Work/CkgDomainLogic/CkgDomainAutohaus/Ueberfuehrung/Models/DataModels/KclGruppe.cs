a3d773148fe2a9ec85184d6e0e9c1def92a01a3c
Matthias Jenzen <matthias.jenzen@kroschke.de>
2014-08-06 10:36:58 +0200

    general assembly rework on Autohaus Überführung

---
+++ Work/CkgDomainLogic/CkgDomainAutohaus/Ueberfuehrung/Models/DataModels/KclGruppe.cs
@@ -0,0 +1,13 @@
+﻿namespace CkgDomainLogic.Ueberfuehrung.Models
+{
+    public class KclGruppe
+    {
+        public string Filiale { get; set; }
+
+        public string Name1 { get; set; }
+
+        public string Strasse { get; set; }
+
+        public string Ort { get; set; }
+    }
+}
