bb3757384e9052e8f1d8d5ef9db5e10403410fff
Matthias Jenzen <matthias.jenzen@kroschke.de>
2014-08-28 12:55:44 +0200

    bing routing optimizations

--- Work/CkgDomainLogic/CkgDomainCommon/DomainCommon/Models/Adresse.cs
+++ Work/CkgDomainLogic/CkgDomainCommon/DomainCommon/Models/Adresse.cs
@@ -13,6 +13,7 @@ namespace CkgDomainLogic.DomainCommon.Models
     public class Adresse : CommonUiModel, IAddressStreetHouseNo, IValidatableObject
     {
         [SelectListKey]
+        [ModelMappingCompareIgnore]
         public string KundenNr { get; set; }
 
         [LocalizedDisplay(LocalizeConstants.Name1)]
