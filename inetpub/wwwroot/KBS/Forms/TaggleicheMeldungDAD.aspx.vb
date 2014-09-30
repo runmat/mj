423667327557fee5574898a277a1f1d494f9f797
Marco Carl <marco.carl@kroschke.de>
2014-09-30 08:40:29 +0200

    Neue Funktion "Taggleiche Meldung DAD" (Sap-Zugriff per SapORM)

---
+++ inetpub/wwwroot/KBS/Forms/TaggleicheMeldungDAD.aspx.vb
@@ -0,0 +1,179 @@
+﻿Imports KBS.KBS_BASE
+Imports KBSBase
+
+Partial Public Class TaggleicheMeldungDAD
+    Inherits Page
+
+    Private mObjKasse As Kasse
+    Dim objMeldungDAD As MeldungDAD
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        FormAuth(Me)
+        lblError.Text = ""
+
+        If mObjKasse Is Nothing Then
+            If Not Session("mKasse") Is Nothing Then
+                mObjKasse = CType(Session("mKasse"), Kasse)
+            Else
+                Throw New Exception("benötigtes Session Objekt nicht vorhanden")
+            End If
+        End If
+
+        Title = lblHead.Text
+
+        If Not IsPostBack Then
+            objMeldungDAD = New MeldungDAD(mObjKasse.Lagerort, mObjKasse.Lagerort)
+            Session("objMeldungDAD") = objMeldungDAD
+
+            lbtnGestern.Attributes.Add("onclick", "SetDate( -1,'" & txtZulassungsdatum.ClientID & "'); return false;")
+            lbtnHeute.Attributes.Add("onclick", "SetDate( 0,'" & txtZulassungsdatum.ClientID & "'); return false;")
+            lbtnMorgen.Attributes.Add("onclick", "SetDate( +1,'" & txtZulassungsdatum.ClientID & "'); return false;")
+
+        ElseIf Session("objMeldungDAD") IsNot Nothing Then
+            objMeldungDAD = CType(Session("objMeldungDAD"), MeldungDAD)
+
+        End If
+    End Sub
+
+    Protected Sub lb_zurueck_Click(ByVal sender As Object, ByVal e As EventArgs) Handles lb_zurueck.Click
+        Response.Redirect("../Selection.aspx")
+    End Sub
+
+    Protected Sub NewSearch_Click(ByVal sender As Object, ByVal e As ImageClickEventArgs) Handles NewSearch.Click
+        Panel1.Visible = Not Panel1.Visible
+        cmdCreate.Visible = Not cmdCreate.Visible
+    End Sub
+
+    Protected Sub cmdCreate_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdCreate.Click, btnEmpty.Click
+        DoSubmit()
+    End Sub
+
+    Private Sub DoSubmit()
+        lblError.Text = ""
+
+        If String.IsNullOrEmpty(txtFahrgestellnummer.Text) AndAlso String.IsNullOrEmpty(txtBriefnummer.Text) Then
+            lblError.Text = "Bitte geben Sie eine Fahrgestell- oder Briefnummer an!"
+            Exit Sub
+        End If
+
+        objMeldungDAD.IDSuche = txtBarcode.Text.Trim()
+        objMeldungDAD.FahrgestellnummerSuche = txtFahrgestellnummer.Text.Trim()
+        objMeldungDAD.BriefnummerSuche = txtBriefnummer.Text.Trim()
+
+        objMeldungDAD.LoadVorgang()
+
+        Session("objMeldungDAD") = objMeldungDAD
+
+        If objMeldungDAD.ErrorOccured Then
+            lblError.Text = "Fehler: " & objMeldungDAD.ErrorMessage
+        Else
+            FillForm()
+        End If
+    End Sub
+
+    Private Sub FillForm()
+        Panel1.Visible = False
+        cmdCreate.Visible = False
+
+        lblIDDisplay.Text = objMeldungDAD.ID
+        lblBestellnummerDisplay.Text = objMeldungDAD.Bestellnummer
+        lblFahrgestellnummerDisplay.Text = objMeldungDAD.Fahrgestellnummer
+        lblBriefnummerDisplay.Text = objMeldungDAD.Briefnummer
+
+        If Not String.IsNullOrEmpty(objMeldungDAD.Zulassungsdatum) Then
+            Dim tmpDate As String = objMeldungDAD.Zulassungsdatum
+            txtZulassungsdatum.Text = tmpDate.Substring(0, 2) & tmpDate.Substring(3, 2) & tmpDate.Substring(8, 2)
+        Else
+            txtZulassungsdatum.Text = ""
+        End If
+
+        If Not String.IsNullOrEmpty(objMeldungDAD.Kennzeichen) Then
+            If objMeldungDAD.Kennzeichen.Contains("-") Then
+                Dim tmpKennz() As String = objMeldungDAD.Kennzeichen.Split("-"c)
+                txtKennz1.Text = ""
+                txtKennz2.Text = ""
+                If tmpKennz.Length = 1 Then
+                    txtKennz1.Text = tmpKennz(0)
+                ElseIf tmpKennz.Length = 2 Then
+                    txtKennz1.Text = tmpKennz(0)
+                    txtKennz2.Text = tmpKennz(1)
+                ElseIf tmpKennz.Length = 3 Then 'Sonderlocke für Behördenfahrzeuge z.B. BWL-4-4444
+                    txtKennz1.Text = tmpKennz(0)
+                    txtKennz2.Text = tmpKennz(1) & "-" & tmpKennz(2)
+                End If
+            Else
+                txtKennz1.Text = objMeldungDAD.Kennzeichen.Substring(0, 3)
+                txtKennz2.Text = objMeldungDAD.Kennzeichen.Substring(2)
+            End If
+        Else
+            txtKennz1.Text = ""
+            txtKennz2.Text = ""
+        End If
+
+        txtGebuehr.Text = ""
+        cbxAuslieferung.Checked = False
+        txtFrachtbriefnummer.Text = objMeldungDAD.Frachtbriefnummer
+
+        Panel2.Visible = True
+        cmdSend.Visible = True
+    End Sub
+
+    Protected Sub cmdFrachtbriefnummer_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdFrachtbriefnummer.Click
+        trFrachtbriefnummer.Visible = True
+    End Sub
+
+    Protected Sub cmdSend_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdSend.Click, btnEmpty2.Click
+        SaveMeldung()
+    End Sub
+
+    Private Sub SaveMeldung()
+        Try
+            If String.IsNullOrEmpty(txtZulassungsdatum.Text) Then
+                lblError.Text = "Bitte geben Sie ein Zulassungsdatum ein."
+                Exit Sub
+            End If
+            objMeldungDAD.Zulassungsdatum = Common.toShortDateStr(txtZulassungsdatum.Text)
+            If String.IsNullOrEmpty(objMeldungDAD.Zulassungsdatum) Then
+                lblError.Text = "Bitte geben Sie ein gültiges Zulassungsdatum ein."
+                Exit Sub
+            End If
+
+            If String.IsNullOrEmpty(txtKennz1.Text) AndAlso String.IsNullOrEmpty(txtKennz2.Text) Then
+                lblError.Text = "Bitte geben Sie ein vollständiges Kennzeichen ein."
+                Exit Sub
+            End If
+            objMeldungDAD.Kennzeichen = txtKennz1.Text.ToUpper() & "-" & txtKennz2.Text.ToUpper()
+
+            If String.IsNullOrEmpty(txtGebuehr.Text) OrElse Not Common.IsDecimal(txtGebuehr.Text.Trim()) Then
+                lblError.Text = "Bitte geben Sie einen Zahlenwert für die Gebühr ein."
+                Exit Sub
+            End If
+            objMeldungDAD.Gebuehr = txtGebuehr.Text.Trim()
+
+            objMeldungDAD.Auslieferung = cbxAuslieferung.Checked
+
+            If (trFrachtbriefnummer.Visible) Then
+                objMeldungDAD.Frachtbriefnummer = txtFrachtbriefnummer.Text
+            End If
+
+            objMeldungDAD.SaveVorgang()
+
+            If objMeldungDAD.ErrorOccured Then
+                lblError.Text = "Fehler beim Absenden: " & objMeldungDAD.ErrorMessage
+            Else
+                lblError.Text = "Die Meldung wurde erfolgreich abgesendet!"
+                Panel2.Visible = False
+                cmdSend.Visible = False
+                Panel1.Visible = True
+                trFrachtbriefnummer.Visible = False
+                cmdCreate.Visible = True
+            End If
+
+            Session("objMeldungDAD") = objMeldungDAD
+
+        Catch ex As Exception
+            lblError.Text = "Fehler beim Absenden: " & ex.Message
+        End Try
+    End Sub
+
+End Class
---
+++ inetpub/wwwroot/KBS/Lib/MeldungDAD.vb
@@ -0,0 +1,117 @@
+﻿Imports System
+Imports KBSBase
+
+''' <summary>
+''' Taggleiche Meldung DAD
+''' </summary>
+''' <remarks></remarks>
+Public Class MeldungDAD
+    Inherits ErrorHandlingClass
+
+#Region "Properties"
+
+    Public Property Kostenstelle As String
+    Public Property Benutzer As String
+    Public Property IDSuche As String
+    Public Property FahrgestellnummerSuche As String
+    Public Property BriefnummerSuche As String
+    Public Property ID As String
+    Public Property Bestellnummer As String
+    Public Property Frachtbriefnummer As String
+    Public Property Fahrgestellnummer As String
+    Public Property Briefnummer As String
+    Public Property Zulassungsdatum As String
+    Public Property Kennzeichen As String
+    Public Property Gebuehr As String
+    Public Property Auslieferung As Boolean
+
+#End Region
+
+#Region "Methods"
+
+    Public Sub New(ByVal kst As String, ByVal user As String)
+        Kostenstelle = kst
+        Benutzer = user
+    End Sub
+
+    ''' <summary>
+    ''' Vorgang zur ID laden. Bapi: Z_ZLD_FIND_DAD_SD_ORDER
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub LoadVorgang()
+        ClearErrorState()
+
+        Try
+            S.AP.Init("Z_ZLD_FIND_DAD_SD_ORDER", "I_VKORG", "1510")
+
+            If Not String.IsNullOrEmpty(IDSuche) Then
+                S.AP.SetImportParameter("I_VBELN", IDSuche.PadLeft(10, "0"c))
+            End If
+
+            If Not String.IsNullOrEmpty(FahrgestellnummerSuche) Then
+                S.AP.SetImportParameter("I_FAHRG", FahrgestellnummerSuche)
+            End If
+
+            If Not String.IsNullOrEmpty(BriefnummerSuche) Then
+                S.AP.SetImportParameter("I_BRIEF", BriefnummerSuche)
+            End If
+
+            Dim tmpTable As DataTable = S.AP.GetExportTableWithExecute("E_VBAK")
+
+            If S.AP.ResultCode = 0 Then
+                Dim row As DataRow = tmpTable.Rows(0)
+                ID = row("VBELN").ToString()
+                Bestellnummer = row("EBELN").ToString()
+                Frachtbriefnummer = row("ZZSEND2").ToString()
+                Fahrgestellnummer = row("ZZFAHRG").ToString()
+                Briefnummer = row("ZZBRIEF").ToString()
+                'TODO: Zulassungsdatum aus SD-Auftrag?
+                Kennzeichen = row("ZZKENN").ToString()
+            Else
+                RaiseError(S.AP.ResultCode.ToString(), S.AP.ResultMessage)
+            End If
+
+        Catch ex As Exception
+            RaiseError("9999", ex.Message)
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Vorgang speichern. Bapi: Z_ZLD_SAVE_TAGGLEICHE_MELDUNG
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub SaveVorgang()
+        ClearErrorState()
+
+        Try
+            Dim tmpTable As DataTable = S.AP.GetImportTableWithInit("Z_ZLD_SAVE_TAGGLEICHE_MELDUNG.IS_TG_MELDUNG")
+
+            Dim newRow As DataRow = tmpTable.NewRow()
+            newRow("VKORG") = "1510"
+            newRow("VBELN") = ID.PadLeft(10, "0"c)
+            newRow("EBELN") = Bestellnummer
+            newRow("FAHRG") = Fahrgestellnummer
+            newRow("BRIEF") = Briefnummer
+            If Common.IsDate(Zulassungsdatum) Then newRow("ZZZLDAT") = Zulassungsdatum
+            newRow("ZZKENN") = Kennzeichen
+            newRow("GEB_AMT") = Gebuehr
+            newRow("AUSLIEF") = Common.BoolToX(Auslieferung)
+            newRow("ZZSEND2") = Frachtbriefnummer
+            newRow("ERNAM") = Benutzer
+            newRow("SAVE_STATUS") = "A"
+            tmpTable.Rows.Add(newRow)
+
+            S.AP.Execute()
+
+            If Not S.AP.ResultCode = 0 Then
+                RaiseError(S.AP.ResultCode.ToString(), S.AP.ResultMessage)
+            End If
+
+        Catch ex As Exception
+            RaiseError("9999", ex.Message)
+        End Try
+    End Sub
+
+#End Region
+
+End Class
