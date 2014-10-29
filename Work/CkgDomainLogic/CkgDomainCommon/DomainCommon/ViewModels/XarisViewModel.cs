6ca2fd6c8b9174cebb1a98cfa70e478410e3af61
Matthias Jenzen <matthias.jenzen@kroschke.de>
2014-10-29 15:26:35 +0100

    Xaris Sepia System integration

--- Work/CkgDomainLogic/CkgDomainCommon/DomainCommon/ViewModels/XarisViewModel.cs
+++ Work/CkgDomainLogic/CkgDomainCommon/DomainCommon/ViewModels/XarisViewModel.cs
@@ -25,7 +25,7 @@ namespace CkgDomainLogic.DomainCommon.ViewModels
 
         private static string XarisAppRelativeUrl { get { return ConfigurationManager.AppSettings["XarisSepiaRelativeUrl"]; } }
 
-        public string XarisUrl { get { return string.Format("{0}{1}?ra={2}&rb={3}", XarisRootUrl, XarisAppRelativeUrl, UserNameHashed, ExpirationToken); } }
+        public string XarisUrl { get { return string.Format("{0}{1}&ra={2}&rb={3}", XarisRootUrl, XarisAppRelativeUrl, UserNameHashed, ExpirationToken); } }
 
 
         public void DataInit()
