49813a469aff679401b7ec9dfd2c47cfc9cbf9f3
Marco Carl <marco.carl@kroschke.de>
2014-11-12 11:28:11 +0100

    Allgemeine Fahrzeughistorie in MVC

---
+++ Work/CkgDomainLogic/CkgDomainEqui/Equi/Models/UiModels/EquiHistorieSuchparameter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CkgDomainLogic.General.Services;
+using GeneralTools.Models;
+using GeneralTools.Resources;
+
+namespace CkgDomainLogic.Equi.Models
+{
+    public class EquiHistorieSuchparameter : IValidatableObject
+    {
+        [LocalizedDisplay(LocalizeConstants.LicenseNo)]
+        public string Kennzeichen { get; set; }
+
+        [LocalizedDisplay(LocalizeConstants.ChassisNo)]
+        public string FahrgestellNr { get; set; }
+
+        [LocalizedDisplay(LocalizeConstants.ZB2)]
+        public string BriefNr { get; set; }
+
+        [LocalizedDisplay(LocalizeConstants.ContractNo)]
+        public string VertragsNr { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if ((String.IsNullOrEmpty(Kennzeichen)) && (String.IsNullOrEmpty(FahrgestellNr)) && (String.IsNullOrEmpty(BriefNr)) && (String.IsNullOrEmpty(VertragsNr)))
+                yield return new ValidationResult(Localize.AtLeastOneSearchCriterionRequired, new[] { "FahrgestellNr" });
+        }
+    }
+}
