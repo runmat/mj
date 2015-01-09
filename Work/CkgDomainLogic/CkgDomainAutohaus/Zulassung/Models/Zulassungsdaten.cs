e543da5b8596c54282c613576c96e0a5398c4cf7
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-01-09 17:56:27 +0100

    - renamed "KroschkeZulassung" to "Autohaus . Zulassung" - first draft of  "Autohaus . ZulassungsReport"

---
+++ Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/Models/Zulassungsdaten.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Xml.Serialization;
+using CkgDomainLogic.General.Services;
+using GeneralTools.Models;
+using GeneralTools.Resources;
+
+namespace CkgDomainLogic.Autohaus.Models
+{
+    public class Zulassungsdaten : IValidatableObject
+    {
+        private string _wunschkennzeichen2;
+        private string _wunschkennzeichen3;
+        private string _kennzeichen;
+
+        [Required]
+        [LocalizedDisplay(LocalizeConstants.RegistrationType)]
+        public string ZulassungsartMatNr { get; set; }
+
+        public Material Zulassungsart
+        {
+            get
+            {
+                if (MaterialList == null)
+                    return new Material();
+
+                var option = MaterialList.FirstOrDefault(m => m.MaterialNr == ZulassungsartMatNr);
+                if (option == null)
+                    return new Material();
+
+                return option;
+            }
+        }
+
+        [XmlIgnore]
+        static public List<Material> MaterialList { get; set; }
+
+        public string Belegtyp { get { return Zulassungsart.Belegtyp; } }
+
+        [Required]
+        [LocalizedDisplay(LocalizeConstants.RegistrationDate)]
+        public DateTime? Zulassungsdatum { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        [LocalizedDisplay(LocalizeConstants.RegistrationDistrict)]
+        public string Zulassungskreis { get; set; }
+
+        [LocalizedDisplay(LocalizeConstants.RegistrationDistrict)]
+        public string ZulassungskreisBezeichnung { get; set; }
+
+        [Required]
+        [LocalizedDisplay(LocalizeConstants.EvbNumber)]
+        public string EvbNr { get; set; }
+
+        [LocalizedDisplay(LocalizeConstants.PersonalisedLicenseNo)]
+        public string Kennzeichen
+        {
+            get { return _kennzeichen.NotNullOrEmpty().ToUpper(); }
+            set { _kennzeichen = value.NotNullOrEmpty().ToUpper(); }
+        }
+
+        [LocalizedDisplay(LocalizeConstants.PersonalisedNumberPlate)]
+        public bool WunschkennzeichenVorhanden
+        {
+            get
+            {
+                return (KennzeichenReserviert ||
+                        (KennzeichenIsValid(Kennzeichen) || KennzeichenIsValid(Wunschkennzeichen2) || KennzeichenIsValid(Wunschkennzeichen3)));
+            }
+        }
+
+        public static string ZulassungskreisToKennzeichenLinkeSeite(string zulassungsKreis)
+        {
+            return string.Format("{0}-", zulassungsKreis.NotNullOrEmpty().ToUpper().RemoveDigits());
+        }
+
+        public static bool KennzeichenIsValid(string kennnzeichen)
+        {
+            return kennnzeichen.IsNotNullOrEmpty() && !kennnzeichen.LastCharIs('-');
+        }
+
+        [LocalizedDisplay(LocalizeConstants.PersonalisedLicenseNo2)]
+        public string Wunschkennzeichen2
+        {
+            get { return _wunschkennzeichen2.NotNullOrEmpty().ToUpper(); }
+            set { _wunschkennzeichen2 = value.NotNullOrEmpty().ToUpper(); }
+        }
+
+        [LocalizedDisplay(LocalizeConstants.PersonalisedLicenseNo3)]
+        public string Wunschkennzeichen3
+        {
+            get { return _wunschkennzeichen3.NotNullOrEmpty().ToUpper(); }
+            set { _wunschkennzeichen3 = value.NotNullOrEmpty().ToUpper(); }
+        }
+
+        [LocalizedDisplay(LocalizeConstants.LicenseNoReserved)]
+        public bool KennzeichenReserviert { get; set; }
+
+        [LocalizedDisplay(LocalizeConstants.ReservationNo)]
+        public string ReservierungsNr { get; set; }
+
+        [LocalizedDisplay(LocalizeConstants.ReservationName)]
+        public string ReservierungsName { get; set; }
+
+        public static bool IstNeuzulassung(string matNr) { return (matNr.TrimStart('0') == "593"); }
+
+        public static bool IstGebrauchtzulassung(string matNr) { return (matNr.TrimStart('0') == "588"); }
+
+        public static bool Ist72hVersandzulassung(string matNr) { return (matNr.TrimStart('0') == "598"); }
+
+        public static bool IstAbmeldung(string matNr) { return (matNr.TrimStart('0') == "573" || matNr.TrimStart('0') == "584" || matNr.TrimStart('0') == "669"); }
+
+        public static bool IstUmkennzeichnung(string matNr) { return (matNr.TrimStart('0') == "596"); }
+
+        public static bool IstKurzzeitzulassung(string matNr) { return (matNr.TrimStart('0') == "592"); }
+
+        public static bool IstFirmeneigeneZulassung(string matNr) { return (matNr.TrimStart('0') == "619"); }
+
+        public static bool IstZollzulassung(string matNr) { return (matNr.TrimStart('0') == "600"); }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!IstKurzzeitzulassung(ZulassungsartMatNr) && !IstZollzulassung(ZulassungsartMatNr) && String.IsNullOrEmpty(EvbNr))
+                yield return new ValidationResult(Localize.EvbNumberRequired, new[] { "EvbNr" });
+
+            if (!String.IsNullOrEmpty(EvbNr) && EvbNr.Length != 7)
+                yield return new ValidationResult(Localize.EvbNumberLengthMustBe7, new[] { "EvbNr" });
+
+            if (KennzeichenReserviert && (String.IsNullOrEmpty(ReservierungsName) || String.IsNullOrEmpty(ReservierungsNr)))
+                yield return new ValidationResult(Localize.ReservationDataRequired, new[] { "ReservierungsName", "ReservierungsNr" });
+        }
+
+        public string GetSummaryString()
+        {
+            var s = "";
+
+            if (Zulassungsart != null)
+                s += String.Format("{0}: {1}", Localize.RegistrationType, Zulassungsart.MaterialText);
+
+            s += String.Format("<br/>{0}: {1}", Localize.RegistrationDate, (Zulassungsdatum.HasValue ? Zulassungsdatum.Value.ToShortDateString() : ""));
+            s += String.Format("<br/>{0}: {1} {2}", Localize.RegistrationDistrict, Zulassungskreis, ZulassungskreisBezeichnung);
+
+            if (!String.IsNullOrEmpty(EvbNr))
+                s += String.Format("<br/>{0}: {1}", Localize.EvbNumber, EvbNr);
+
+            s += String.Format("<br/>{0}: {1}", Localize.PersonalisedLicenseNo, Kennzeichen);
+
+            if (KennzeichenReserviert)
+            {
+                s += String.Format("<br/>{0}", Localize.LicenseNoReserved);
+                s += String.Format("<br/>{0}: {1}", Localize.ReservationNo, ReservierungsNr);
+                s += String.Format("<br/>{0}: {1}", Localize.ReservationName, ReservierungsName);
+            }
+            else
+            {
+                s += String.Format("<br/>{0}: {1}", Localize.PersonalisedLicenseNo2, Wunschkennzeichen2);
+                s += String.Format("<br/>{0}: {1}", Localize.PersonalisedLicenseNo3, Wunschkennzeichen3);
+            }
+
+            return s;
+        }
+    }
+}
---
+++ inetpub/wwwroot/ServicesMvc/Areas/Autohaus/Controllers/ZulassungController.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using CkgDomainLogic.DomainCommon.Models;
+using CkgDomainLogic.Fahrzeugbestand.Contracts;
+using CkgDomainLogic.General.Contracts;
+using CkgDomainLogic.General.Controllers;
+using CkgDomainLogic.General.Services;
+using CkgDomainLogic.Autohaus.Contracts;
+using CkgDomainLogic.Autohaus.Models;
+using CkgDomainLogic.Autohaus.ViewModels;
+using CkgDomainLogic.Partner.Contracts;
+using DocumentTools.Services;
+using GeneralTools.Contracts;
+using GeneralTools.Models;
+using MvcTools.Web;
+using Telerik.Web.Mvc;
+
+namespace ServicesMvc.Autohaus.Controllers
+{
+    public class ZulassungController : CkgDomainController 
+    {
+        public override string DataContextKey { get { return GetDataContextKey<KroschkeZulassungViewModel>(); } }
+
+        public KroschkeZulassungViewModel ViewModel 
+        { 
+            get { return GetViewModel<KroschkeZulassungViewModel>(); } 
+            set { SetViewModel(value); } 
+        }
+
+        public ZulassungController(IAppSettings appSettings, ILogonContextDataService logonContext,
+            IPartnerDataService partnerDataService,
+            IZulassungDataService zulassungDataService,
+            IFahrzeugAkteBestandDataService fahrzeugbestandDataService)
+            : base(appSettings, logonContext)
+        {
+            if (IsInitialRequestOf("Index"))
+                ViewModel = null;
+
+            InitViewModelExpicit(ViewModel, appSettings, logonContext, partnerDataService, zulassungDataService, fahrzeugbestandDataService);
+        }
+
+        private void InitViewModelExpicit(KroschkeZulassungViewModel vm, IAppSettings appSettings, ILogonContextDataService logonContext, IPartnerDataService partnerDataService, IZulassungDataService zulassungDataService, IFahrzeugAkteBestandDataService fahrzeugbestandDataService)
+        {
+            InitViewModel(vm, appSettings, logonContext, partnerDataService, zulassungDataService, fahrzeugbestandDataService);
+        }
+
+        [CkgApplication]
+        public ActionResult Index(string fin, string halterNr)
+        {
+            ViewModel.DataInit();
+
+            ViewModel.SetParamFahrzeugAkte(fin);
+            
+            if (halterNr.IsNotNullOrEmpty())
+                ViewModel.SetParamHalter(halterNr);
+
+            ShoppingCartLoadAndCacheItems();
+            ShoppingCartTryEditItemAsViewModel();
+
+            return View(ViewModel);
+        }
+
+
+        #region Rechnungsdaten
+
+        [HttpPost]
+        public ActionResult Rechnungsdaten()
+        {
+            return PartialView("Partial/Rechnungsdaten", ViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult RechnungsdatenForm(Rechnungsdaten model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewModel.SetRechnungsdaten(model);
+            }
+
+            return PartialView("Partial/RechnungsdatenForm", model);
+        }
+
+        #endregion
+
+        #region Bank-/Adressdaten
+
+        [HttpPost]
+        public ActionResult BankAdressdaten()
+        {
+            ViewModel.CheckCpd();
+
+            return PartialView("Partial/BankAdressdaten", ViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult BankAdressdatenForm(BankAdressdaten model)
+        {
+            ViewModel.SetBankAdressdaten(ref model);
+
+            return PartialView("Partial/BankAdressdatenForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult LoadBankdatenAusIban(string iban)
+        {
+            var bankdaten = ViewModel.LoadBankdatenAusIban(iban);
+
+// ReSharper disable RedundantAnonymousTypePropertyName
+            return Json(new { Swift = bankdaten.Swift, KontoNr = bankdaten.KontoNr, Bankleitzahl = bankdaten.Bankleitzahl, Geldinstitut = bankdaten.Geldinstitut });
+// ReSharper restore RedundantAnonymousTypePropertyName
+        }
+
+        #endregion
+
+        #region Fahrzeugdaten
+
+        [HttpPost]
+        public ActionResult Fahrzeugdaten()
+        {
+            return PartialView("Partial/Fahrzeugdaten", ViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult FahrzeugdatenForm(Fahrzeugdaten model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewModel.SetFahrzeugdaten(model);
+            }
+
+            return PartialView("Partial/FahrzeugdatenForm", model);
+        }
+
+        #endregion
+
+        #region HalterAdresse
+
+        [HttpPost]
+        public ActionResult HalterAdresse()
+        {
+            return PartialView("Partial/HalterAdresse", ViewModel);
+        }
+
+        [HttpPost]
+        public JsonResult HalterAdresseGetAutoCompleteItems()
+        {
+            return Json(new { items = ViewModel.GetHalterAdressenAsAutoCompleteItems() });
+        }
+
+        [HttpPost]
+        public ActionResult HalterAdresseForm(Adresse model)
+        {
+            if (model.TmpSelectionKey.IsNotNullOrEmpty())
+            {
+                model = ViewModel.GetHalteradresse(model.TmpSelectionKey);
+                if (model == null)
+                    return new EmptyResult();
+
+                ModelState.Clear();
+                model.IsValid = false;
+                return PartialView("Partial/HalterAdresseForm", model);
+            }
+
+            if (ModelState.IsValid)
+                ViewModel.SetHalterAdresse(model);
+
+            model.IsValid = ModelState.IsValid;
+
+            return PartialView("Partial/HalterAdresseForm", model);
+        }
+
+        [GridAction]
+        public ActionResult HalterAdressenAjaxBinding()
+        {
+            var items = ViewModel.HalterAdressenFiltered;
+            return View(new GridModel(items));
+        }
+
+        [HttpPost]
+        public ActionResult FilterHalterAdressenAuswahlGrid(string filterValue, string filterColumns)
+        {
+            ViewModel.FilterHalterAdressen(filterValue, filterColumns);
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult HalterAdressenShowGrid()
+        {
+            ViewModel.DataMarkForRefreshHalterAdressen();
+
+            return PartialView("Partial/HalterAdressenAuswahlGrid");
+        }
+
+        public ActionResult HalterAdressenAuswahlExportFilteredExcel(int page, string orderBy, string filterBy)
+        {
+            var dt = ViewModel.HalterAdressenFiltered.GetGridFilteredDataTable(orderBy, filterBy, LogonContext.CurrentGridColumns);
+            new ExcelDocumentFactory().CreateExcelDocumentAndSendAsResponse("HalterAdressen", dt);
+
+            return new EmptyResult();
+        }
+
+        public ActionResult HalterAdressenAuswahlExportFilteredPDF(int page, string orderBy, string filterBy)
+        {
+            var dt = ViewModel.HalterAdressenFiltered.GetGridFilteredDataTable(orderBy, filterBy, LogonContext.CurrentGridColumns);
+            new ExcelDocumentFactory().CreateExcelDocumentAsPDFAndSendAsResponse("HalterAdressen", dt, landscapeOrientation: true);
+
+            return new EmptyResult();
+        }
+
+        #endregion
+        
+        #region Zulassungsdaten
+
+        [HttpPost]
+        public ActionResult Zulassungsdaten()
+        {
+            return PartialView("Partial/Zulassungsdaten", ViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult ZulassungsdatenForm(Zulassungsdaten model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewModel.SetZulassungsdaten(model);
+            }
+
+            return PartialView("Partial/ZulassungsdatenForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult LoadKfzKreisAusHalterAdresse()
+        {
+            return Json(new { kfzKreis = ViewModel.LoadKfzKreisAusHalterAdresse() });
+        }
+
+        [HttpPost]
+        public ActionResult GetKennzeichenLinkeSeite(string zulassungsKreis)
+        {
+            return Json(new { kennzeichenLinkeSeite = ViewModel.ZulassungskreisToKennzeichenLinkeSeite(zulassungsKreis) });
+        }
+
+        #endregion
+
+        #region OptionenDienstleistungen
+
+        [HttpPost]
+        public ActionResult OptionenDienstleistungen()
+        {
+            ViewModel.Zulassung.OptionenDienstleistungen.InitDienstleistungen();
+
+            return PartialView("Partial/OptionenDienstleistungen", ViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult OptionenDienstleistungenForm(OptionenDienstleistungen model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewModel.SetOptionenDienstleistungen(model);
+            }
+
+            return PartialView("Partial/OptionenDienstleistungenForm", ViewModel.Zulassung.OptionenDienstleistungen);
+        }
+
+        #endregion
+
+        #region Summary + Receipt
+
+        [HttpPost]
+        public ActionResult Save()
+        {
+            ViewModel.Save(new List<Vorgang> { ViewModel.Zulassung }, saveDataToSap: false, saveFromShoppingCart: false);
+
+            ShoppingCartItemSave();
+
+            return PartialView("Partial/Receipt", ViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Receipt()
+        {
+            ViewModel.Save(new List<Vorgang> { ViewModel.Zulassung }, saveDataToSap: true, saveFromShoppingCart: false);
+            ShoppingCartItemRemove(ViewModel.ObjectKey);
+
+            return PartialView("Partial/Receipt", ViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Summary()
+        {
+            return PartialView("Partial/Summary", ViewModel.Zulassung.CreateSummaryModel());
+        }
+
+        public FileContentResult SummaryAsPdf(string id)
+        {
+            var zulassung = ViewModel.ZulassungenForReceipt.FirstOrDefault(z => z.BelegNr == id);
+            if (zulassung == null)
+                return new FileContentResult(new byte[1], "");
+
+            var summaryHtml = this.RenderPartialViewToString("Partial/SummaryPdf", zulassung.CreateSummaryModel());
+
+            var summaryPdfBytes = PdfDocumentFactory.HtmlToPdf(summaryHtml);
+
+            return new FileContentResult(summaryPdfBytes, "application/pdf") { FileDownloadName = String.Format("{0}.pdf", Localize.Overview) };
+        }
+
+        public FileContentResult KundenformularAsPdf(string id)
+        {
+            var zulassung = ViewModel.ZulassungenForReceipt.FirstOrDefault(z => z.BelegNr == id);
+            if (zulassung == null)
+                return new FileContentResult(new byte[1], "");
+
+            var formularPdfBytes = zulassung.KundenformularPdf;
+
+            return new FileContentResult(formularPdfBytes, "application/pdf") { FileDownloadName = String.Format("{0}.pdf", Localize.CustomerForm) };
+        }
+
+        public FileContentResult ZusatzformularAsPdf(string id, string typ)
+        {
+            var zulassung = ViewModel.ZulassungenForReceipt.FirstOrDefault(z => z.BelegNr == id);
+            if (zulassung == null)
+                return new FileContentResult(new byte[1], "");
+
+            var zusatzFormular = zulassung.Zusatzformulare.FirstOrDefault(z => z.Typ == typ);
+            if (zusatzFormular == null)
+                return new FileContentResult(new byte[1], ""); 
+
+            var auftragPdfBytes = System.IO.File.ReadAllBytes(zusatzFormular.DateiPfad);
+
+            return new FileContentResult(auftragPdfBytes, "application/pdf") { FileDownloadName = String.Format("{0}.pdf", Localize.OrderForm) };
+        }
+
+        public FileContentResult AuftragslisteAsPdf()
+        {
+            var zulassung = ViewModel.ZulassungenForReceipt.FirstOrDefault();
+            if (zulassung == null)
+                return new FileContentResult(new byte[1], "");
+
+            var auftragslisteFormular = zulassung.Zusatzformulare.FirstOrDefault(z => z.IstAuftragsListe);
+            if (auftragslisteFormular == null)
+                return new FileContentResult(new byte[1], "");
+            
+            var auftragPdfBytes = System.IO.File.ReadAllBytes(auftragslisteFormular.DateiPfad);
+
+            return new FileContentResult(auftragPdfBytes, "application/pdf") { FileDownloadName = String.Format("{0}.pdf", Localize.OrderList) };
+        }
+
+        #endregion   
+
+
+        #region Shopping Cart 
+
+        private const string ShoppingCartPersistanceKey = "KroschkeZulassung";
+
+        protected override IEnumerable ShoppingCartLoadItems()
+        {
+            return ShoppingCartLoadGenericItems<KroschkeZulassungViewModel>(ShoppingCartPersistanceKey);
+        }
+
+        protected void ShoppingCartTryEditItemAsViewModel()
+        {
+            var objectKey = ShoppingCartPopEditItemKey();
+            if (objectKey == null)
+                return;
+
+            var vm = ShoppingCartGetItem(objectKey) as KroschkeZulassungViewModel;
+            if (vm == null)
+                return;
+
+            InitViewModelExpicit(vm, AppSettings, LogonContext, ViewModel.PartnerDataService, ViewModel.ZulassungDataService, ViewModel.FahrzeugAkteBestandDataService);
+            vm.DataMarkForRefresh();
+            ViewModel = vm;
+        }
+
+        private void ShoppingCartItemSave()
+        {
+            ShoppingCartSaveItem(ShoppingCartPersistanceKey, ViewModel);
+        }
+
+        protected override void ShoppingCartFilterItems(string filterValue, string filterProperties)
+        {
+            ShoppingCartFilterGenericItems<KroschkeZulassungViewModel>(filterValue, filterProperties);
+        }
+
+        [HttpPost]
+        public override ActionResult ShoppingCartSelectedItemsSubmit()
+        {
+            var warenkorb = ShoppingCartItems.Cast<KroschkeZulassungViewModel>().Where(item => item.IsSelected).ToListOrEmptyList();
+            foreach (var vm in warenkorb)
+            {
+                InitViewModelExpicit(vm, AppSettings, LogonContext, ViewModel.PartnerDataService, ViewModel.ZulassungDataService, ViewModel.FahrzeugAkteBestandDataService);
+                vm.DataMarkForRefresh();
+            }
+
+            ViewModel.Save(warenkorb.Select(wk => wk.Zulassung).ToListOrEmptyList(), saveDataToSap: true, saveFromShoppingCart: true);
+
+            if (ViewModel.SaveErrorMessage.IsNullOrEmpty())
+            {
+                foreach (var vm in warenkorb)
+                    ShoppingCartItemRemove(vm.ObjectKey);
+            }
+
+            return PartialView("Partial/Receipt", ViewModel);
+        }
+
+        #endregion
+    }
+}
