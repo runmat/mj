547ecbac574623c2d559fc8ce68c3efe8fffbd2d
Marco Carl <marco.carl@kroschke.de>
2015-01-09 13:18:07 +0100

    SapORM-Model für Z_AHP_READ_TYPDAT_BESTAND aktualisiert

--- Work/SapORM/shell/App.xaml.cs
+++ Work/SapORM/shell/App.xaml.cs
@@ -248,7 +248,7 @@ namespace SapORM
 
             //new FunctionReflector("Z_AHP_CRE_CHG_FZG_AKT_BEST").WriteOrmForExportTableStructures("I_KUNNR", "10026883");
             //new FunctionReflector("Z_AHP_READ_FZGBESTAND").WriteOrmForExportTableStructures("I_KUNNR", "10026883");
-            //ok new FunctionReflector("Z_AHP_READ_TYPDAT_BESTAND").WriteOrmForExportTableStructures("I_KUNNR, I_FIN, I_ZZHERSTELLER_SCH, I_ZZTYP_SCHL, I_ZZVVS_SCHLUESSEL", "10026883", "9876543210", "X", "X", "X"); 
+            //new FunctionReflector("Z_AHP_READ_TYPDAT_BESTAND").WriteOrmForExportTableStructures("I_KUNNR", "10026883"); 
 
             //CoCTest();
             //TeslaTest();
