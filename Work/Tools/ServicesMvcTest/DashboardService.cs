06271b80bbe12fbe9920742b3f13ad6ab904ab7c
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-01-19 17:17:07 +0100

    first draft

---
+++ Work/Tools/ServicesMvcTest/DashboardService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using GeneralTools.Models;
+using ServicesMvc;
+
+namespace CkgDomainLogic.Services
+{
+    public class DashboardService
+    {
+        public static void InvokeViewModelForAppUrl(string appUrl)
+        {
+            string area, controller, action;
+            GetAppUrlParts(appUrl, out area, out controller, out action);
+
+            var servicesMvcAssembly = Assembly.GetAssembly(typeof (MvcApplication));
+
+            var controllerType = GetControllerType(area, controller, servicesMvcAssembly);
+            if (controllerType == null)
+                return;
+
+
+
+        }
+
+        private static Type GetControllerType(string area, string controller, Assembly servicesMvcAssembly)
+        {
+            var controllerTypeName = string.Format("servicesmvc.{0}controllers.{1}controller", (area.IsNullOrEmpty() ? "" : area + "."), controller);
+            var controllerType = servicesMvcAssembly.GetType(controllerTypeName, throwOnError: false, ignoreCase: true);
+
+            return controllerType;
+        }
+
+        private static string GetAppUrlParts(string appUrl, out string area, out string controller, out string action)
+        {
+            appUrl = appUrl.NotNullOrEmpty().ToLower();
+            if (appUrl.StartsWith("mvc/"))
+                appUrl = appUrl.SubstringTry(4);
+
+            var urlParts = appUrl.Split('/');
+            area = "";
+            var index = -1;
+            if (urlParts.Count() > 1)
+                area = urlParts[++index];
+            controller = urlParts[++index];
+            action = urlParts[++index];
+
+            return appUrl;
+        }
+    }
+}
--- Work/Tools/ServicesMvcTest/Program.cs
+++ Work/Tools/ServicesMvcTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CkgDomainLogic.Services;
 
 namespace ServicesMvcTest
 {
@@ -9,6 +10,7 @@ namespace ServicesMvcTest
     {
         static void Main(string[] args)
         {
+            DashboardService.InvokeViewModelForAppUrl("mvc/Autohaus/ZulassungsReport/Index");
         }
     }
 }
