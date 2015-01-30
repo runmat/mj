c4eaec78d1d14bcdbd8c212a42aaa8423982f311
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-01-30 17:21:25 +0100

    added support for pie charts

--- Work/CkgDomainLogic/Lib/General/Services/Dashboard/ChartService.cs
+++ Work/CkgDomainLogic/Lib/General/Services/Dashboard/ChartService.cs
@@ -8,33 +8,65 @@ namespace CkgDomainLogic.General.Services
 {
     public class ChartService
     {
-        public static ChartItemsPackage GetGroupedAndStackedItemsWithLabels<T>
+        public static ChartItemsPackage GetPieChartGroupedItemsWithLabels<T>
             (
                 IList<T> items,
                 Func<T, string> xAxisKey,
-                Action<IList<string>> addAdditionalXaxisKeys = null,
-                IEnumerable<string> stackedGroupValues = null,
-                Func<T, string> stackedKey = null,
                 Func<IGrouping<int, T>, int> aggregate = null
             ) where T : class
         {
-            var xAxisList = items.GroupBy(xAxisKey).OrderBy(k => k.Key).Select(k => k.Key).ToListOrEmptyList();
+            var xAxisList = items.OrderBy(xAxisKey).GroupBy(xAxisKey).Select(k => k.Key).ToListOrEmptyList();
+            var xAxisLabels = xAxisList.ToArray();
+
+            var groupArray = items
+                .OrderBy(xAxisKey)
+                .GroupBy(group => xAxisList.IndexOf(xAxisKey(group)))
+                .Select(g => new[] { g.Key, (aggregate == null ? g.Count() : aggregate(g)) })
+                .ToArray();
+
+            var data = new object[groupArray.Count()];
+            for (var k = 0; k < groupArray.Count(); k++)
+            {
+                data[k] = new
+                    {
+                        data = new [] { groupArray[k] },
+                        label = xAxisLabels[k]
+                    };
+            }
+
+            return new ChartItemsPackage
+            {
+                data = data
+            };
+        }
+
+        public static ChartItemsPackage GetBarChartGroupedStackedItemsWithLabels<T>
+            (
+            IList<T> items,
+            Func<T, string> xAxisKey,
+            Action<IList<string>> addAdditionalXaxisKeys = null,
+            IEnumerable<string> stackedGroupValues = null,
+            Func<T, string> stackedKey = null,
+            Func<IGrouping<int, T>, int> aggregate = null
+            ) where T : class
+        {
+            var xAxisList = items.OrderBy(xAxisKey).GroupBy(xAxisKey).Select(k => k.Key).ToListOrEmptyList();
             if (xAxisList.Any() && addAdditionalXaxisKeys != null)
                 addAdditionalXaxisKeys(xAxisList);
             var xAxisLabels = xAxisList.ToArray();
 
             if (stackedGroupValues == null)
-                stackedGroupValues = new []{""};
+                stackedGroupValues = new[] { "" };
 
             if (stackedKey == null)
                 stackedKey = e => "";
 
-            var groupValuesArray = stackedGroupValues.ToArray();
+            var stackedGroupValuesArray = stackedGroupValues.ToArray();
             var data = new object[stackedGroupValues.Count()];
             for (var k = 0; k < stackedGroupValues.Count(); k++)
             {
                 var subArray = items
-                    .Where(item => stackedKey(item) == groupValuesArray[k])
+                    .Where(item => stackedKey(item) == stackedGroupValuesArray[k])
                     .GroupBy(group => xAxisList.IndexOf(xAxisKey(group)))
                     .Select(g => new[] { g.Key, (aggregate == null ? g.Count() : aggregate(g)) })
                     .ToArray();
@@ -42,7 +74,7 @@ namespace CkgDomainLogic.General.Services
                 data[k] = new
                 {
                     data = subArray,
-                    label = groupValuesArray[k]
+                    label = stackedGroupValuesArray[k]
                 };
             }
 
