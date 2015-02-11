c6877c53eba6ceb328183e88a32ba675add96d70
Marco Carl <marco.carl@kroschke.de>
2015-02-11 07:11:33 +0100

    Cherry Pick aus ITA 7751: SapORM um Verarbeitung von Import-Strukturen erweitert

--- Work/SapORM/DynSapProxy/ProxyObject/DynSapProxyObjectErp35/DynSapProxyObjectErp35.cs
+++ Work/SapORM/DynSapProxy/ProxyObject/DynSapProxyObjectErp35/DynSapProxyObjectErp35.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.Serialization;
 using GeneralTools.Contracts;
+using GeneralTools.Models;
 using GeneralTools.Services;
 using Microsoft.VisualBasic;
 using SapORM.Contracts;
@@ -23,6 +24,16 @@ namespace SapORM.Services
 	    private bool _clearMe;
 
 		private ISapConnection _sapConnection;
+
+        private string[] _acceptableErpExceptions = new string[]
+            {
+                "NRF",
+                "NO_DATA",
+                "NO_RESULT",
+                "ERR_NO_DATA",
+                "ERR_DAT",
+                "NO_HEADER"
+            };
 
 	    public string BapiName { get; set; }
 
@@ -91,7 +102,7 @@ namespace SapORM.Services
 
 
 
-        public bool CallBapi(ILogService logService = null, ILogonContext logonContext = null)
+        public bool CallBapi(ILogService logService = null, ILogonContext logonContext = null, bool modelGenerationMode = false)
 		{
 		    var itemsCollect = new RFCTableCollection();
 
@@ -279,7 +290,25 @@ namespace SapORM.Services
 
                 stopwatch = Stopwatch.StartNew();
 
+                try
+                {
 				func.Execute();
+                }
+                catch (ERPException erpEx)
+                {
+                    // No-Data-Exceptions sollen nicht zu Exceptions führen
+                    if (erpEx.Source.Contains("ERPConnect35")
+                        && !String.IsNullOrEmpty(erpEx.ABAPException)
+                        && Array.IndexOf(_acceptableErpExceptions, erpEx.ABAPException.ToUpper()) > -1)
+                    {
+                        if (!modelGenerationMode)
+                            return false;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
 
                 if (stopwatch != null)
                     stopwatch.Stop();
@@ -289,9 +318,12 @@ namespace SapORM.Services
                 //Export-Tabellen
 				foreach (var tmpRow in Export.Select("ElementCode='TABLE'")) {
 					// Exportparameter Struktur oder Tabelle
-					foreach (RFCParameter it in func.Imports) {
-						if (it.IsStructure()) {
-							if (it.Name == ((DataTable)tmpRow[0]).TableName) {
+                    foreach (RFCParameter it in func.Imports)
+                    {
+                        if (it.IsStructure())
+                        {
+                            if (it.Name == ((DataTable)tmpRow[0]).TableName)
+                            {
 								item = it.ToStructure();
 							    var tblTemp = new DataTable {TableName = it.Name};
 							    foreach (RFCTableColumn col in item.Columns) {
@@ -299,8 +331,10 @@ namespace SapORM.Services
 								}
 							    var row = tblTemp.NewRow();
 
-								foreach (RFCTableColumn col in item.Columns) {
-									switch (col.Type) {
+                                foreach (RFCTableColumn col in item.Columns)
+                                {
+                                    switch (col.Type)
+                                    {
 										case RFCTYPE.NUM:
 											row[col.Name] = item[col.Name].ToString();
 											break;
@@ -308,21 +342,28 @@ namespace SapORM.Services
 											row[col.Name] = item[col.Name].ToString();
 											break;
 										case RFCTYPE.BCD:
-											if (ReferenceEquals(item[col.Name], DBNull.Value)) {
+                                            if (ReferenceEquals(item[col.Name], DBNull.Value))
+                                            {
 												row[col.Name] = "";
 											}
 											break;
 										case RFCTYPE.DATE:
-											if (item[col.Name].ToString() == "00000000" || string.IsNullOrEmpty(item[col.Name].ToString())) {
+                                            if (item[col.Name].ToString() == "00000000" || string.IsNullOrEmpty(item[col.Name].ToString()))
+                                            {
 												row[col.Name] = "";
-											} else {
+                                            }
+                                            else
+                                            {
 												row[col.Name] = ConversionUtils.SAPDate2NetDate(item[col.Name].ToString());
 											}
 											break;
 										case RFCTYPE.TIME:
-											if (item[col.Name].ToString() == "000000" || string.IsNullOrEmpty(item[col.Name].ToString())) {
+                                            if (item[col.Name].ToString() == "000000" || string.IsNullOrEmpty(item[col.Name].ToString()))
+                                            {
 												row[col.Name] = "";
-											} else {
+                                            }
+                                            else
+                                            {
 												row[col.Name] = item[col.Name].ToString();
 											}
 											break;
@@ -335,16 +376,22 @@ namespace SapORM.Services
 								tblTemp.Rows.Add(row);
 								tblTemp.AcceptChanges();
 								tmpRow[0] = tblTemp;
-							} else if (it.IsTable()) {
+                            }
+                            else if (it.IsTable())
+                            {
 								var rfcT = it.ToTable();
 
-								if (rfcT.Name == ((DataTable)tmpRow[0]).TableName) {
+                                if (rfcT.Name == ((DataTable)tmpRow[0]).TableName)
+                                {
 									var tblTemp = rfcT.ToADOTable();
 
 
-									foreach (RFCTableColumn col in rfcT.Columns) {
-										foreach (DataRow row in tblTemp.Rows) {
-											switch (col.Type) {
+                                    foreach (RFCTableColumn col in rfcT.Columns)
+                                    {
+                                        foreach (DataRow row in tblTemp.Rows)
+                                        {
+                                            switch (col.Type)
+                                            {
 
 												case RFCTYPE.NUM:
 													row[col.Name] = row[col.Name].ToString();
@@ -354,14 +401,18 @@ namespace SapORM.Services
 													row[col.Name] = row[col.Name].ToString();
 													break;
 												case RFCTYPE.BCD:
-													if (ReferenceEquals(row[col.Name], DBNull.Value)) {
+                                                    if (ReferenceEquals(row[col.Name], DBNull.Value))
+                                                    {
 														row[col.Name] = "";
 													}
 													break;
 												case RFCTYPE.DATE:
-													if (row[col.Name].ToString() == "00000000" || string.IsNullOrEmpty(row[col.Name].ToString())) {
+                                                    if (row[col.Name].ToString() == "00000000" || string.IsNullOrEmpty(row[col.Name].ToString()))
+                                                    {
 														row[col.Name] = "";
-													} else {
+                                                    }
+                                                    else
+                                                    {
 														row[col.Name] = ConversionUtils.SAPDate2NetDate(row[col.Name].ToString());
 													}
 													break;
@@ -370,9 +421,12 @@ namespace SapORM.Services
 
 													break;
 												case RFCTYPE.TIME:
-													if (row[col.Name].ToString() == "000000" || string.IsNullOrEmpty(row[col.Name].ToString())) {
+                                                    if (row[col.Name].ToString() == "000000" || string.IsNullOrEmpty(row[col.Name].ToString()))
+                                                    {
 														row[col.Name] = "";
-													} else {
+                                                    }
+                                                    else
+                                                    {
 														row[col.Name] = row[col.Name].ToString();
 													}
 													break;
@@ -455,7 +509,6 @@ namespace SapORM.Services
 			} 
             catch (Exception generalException)
             {
-
                 double callduration = -1;
                 if (stopwatch != null && stopwatch.IsRunning)
                 {
@@ -463,46 +516,13 @@ namespace SapORM.Services
                     callduration = stopwatch.Elapsed.TotalSeconds;
                 }
 
-
                 PreserveStackTrace(generalException);
 
                 LogSapBapiCall(logService, logonContext, true, callduration);
 
-                // ERPException NRF, NO_DATA, ERR_NO_DATA, und ERR_DATA hier behandeln und den Aufrufer einen false zurückgeben
-                // Alle anderen Exceptions NICHT hier behandeln und weitergeben
-                // Anstelle von throw new Exception(generalException.Message); komme ich hier mit einem throw aus
-                // - der original Exception ist erhalten und wird von der Fehlerbehandlung abgefangen
-                // - der original Stacktrace ist erhalten (PreserveStackTrace ist aufgerufen worden) 
-                var ex = generalException as ERPException;
-			    if (ex != null && ex.Source.Contains("ERPConnect35")) {
-					if (ex.ABAPException.Length > 0) {
-                        if (ex.ABAPException.ToUpper() == "NRF")
-                            return false;
-                        if (ex.ABAPException.ToUpper() == "NO_DATA")
-                            return false;
-                        if (ex.ABAPException.ToUpper() == "NO_RESULT")
-                            return false;
-                        if (ex.ABAPException.ToUpper() == "ERR_NO_DATA")
-                            return false;
-                        if (ex.ABAPException.ToUpper() == "ERR_DAT")
-                            return false;
-
 					    
 						throw;
 					} 
-                    else if (ex.Message.Contains("Connect to SAP gateway failed")) {
-                        
-                        throw;
-					} 
-                    else
-                    {
-                        throw;
-					}
-				} 
-                else {
-                    throw;
-				}
-			} 
             finally 
             {
                 if ((con != null)) {
