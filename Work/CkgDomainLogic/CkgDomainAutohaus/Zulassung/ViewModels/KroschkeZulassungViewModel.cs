d50e19e64ae94911d64c41a0ae95555518a9b92b
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-02-17 10:47:42 +0100

    Register "Halter" now again necessary for mode "Abmeldungen"

--- Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/ViewModels/KroschkeZulassungViewModel.cs
+++ Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/ViewModels/KroschkeZulassungViewModel.cs
@@ -70,7 +70,7 @@ namespace CkgDomainLogic.Autohaus.ViewModels
                                 return;
                             }
 
-                            if (entry.Key == "OptionenDienstleistungen" || entry.Key == "HalterAdresse")
+                            if (entry.Key == "OptionenDienstleistungen")
                                 return;
 
                             abmeldungsDict.Add(entry.Key, entry.Value);
