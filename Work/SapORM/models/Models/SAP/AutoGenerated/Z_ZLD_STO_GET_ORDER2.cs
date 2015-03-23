66eb2f9866c2af63ad7cdc4dd454d78d016321c5
Marco Carl <marco.carl@kroschke.de>
2015-03-23 08:29:04 +0100

    SapORM-Models aktualisiert

--- Work/SapORM/models/Models/SAP/AutoGenerated/Z_ZLD_STO_GET_ORDER2.cs
+++ Work/SapORM/models/Models/SAP/AutoGenerated/Z_ZLD_STO_GET_ORDER2.cs
@@ -31,8 +31,6 @@ namespace SapORM.Models
 			[ScriptIgnore]
 			public IDynSapProxyFactory DynSapProxyFactory { get; set; }
 
-			public string MANDT { get; set; }
-
 			public string ZULBELN { get; set; }
 
 			public string VBELN { get; set; }
@@ -43,15 +41,13 @@ namespace SapORM.Models
 
 			public DateTime? VE_ERDAT { get; set; }
 
-			public string VE_ERZEIT { get; set; }
-
 			public string VE_ERNAM { get; set; }
+
+			public string VE_ERZEIT { get; set; }
 
 			public DateTime? ERDAT { get; set; }
 
 			public string ERNAM { get; set; }
-
-			public string FLAG { get; set; }
 
 			public string STATUS { get; set; }
 
@@ -67,27 +63,9 @@ namespace SapORM.Models
 
 			public string KUNNR { get; set; }
 
-			public string KUNNR_NEU { get; set; }
-
 			public string ZZREFNR1 { get; set; }
 
 			public string ZZREFNR2 { get; set; }
-
-			public string ZZREFNR3 { get; set; }
-
-			public string ZZREFNR4 { get; set; }
-
-			public string ZZREFNR5 { get; set; }
-
-			public string ZZREFNR6 { get; set; }
-
-			public string ZZREFNR7 { get; set; }
-
-			public string ZZREFNR8 { get; set; }
-
-			public string ZZREFNR9 { get; set; }
-
-			public string ZZREFNR10 { get; set; }
 
 			public string KREISKZ { get; set; }
 
@@ -99,25 +77,15 @@ namespace SapORM.Models
 
 			public string RESERVKENN { get; set; }
 
-			public string FEINSTAUBAMT { get; set; }
-
 			public DateTime? ZZZLDAT { get; set; }
 
 			public string ZZKENN { get; set; }
-
-			public string WU_KENNZ2 { get; set; }
-
-			public string WU_KENNZ3 { get; set; }
-
-			public string KENNZTYP { get; set; }
 
 			public string KENNZFORM { get; set; }
 
 			public string KENNZANZ { get; set; }
 
 			public string EINKENN_JN { get; set; }
-
-			public string ZUSKENNZ { get; set; }
 
 			public string BEMERKUNG { get; set; }
 
@@ -127,137 +95,51 @@ namespace SapORM.Models
 
 			public string RE_JN { get; set; }
 
-			public string POOLNR { get; set; }
-
-			public string ZL_RL_FRBNR_HIN { get; set; }
-
-			public string ZL_RL_FRBNR_ZUR { get; set; }
-
 			public string ZL_LIFNR { get; set; }
 
 			public string KUNDEBAR_JN { get; set; }
-
-			public string ZAHLART { get; set; }
 
 			public string LOEKZ { get; set; }
 
 			public string KSTATUS { get; set; }
 
-			public string BARQ_NR { get; set; }
-
-			public string VK_KUERZEL { get; set; }
-
-			public string KUNDEN_REF { get; set; }
-
-			public string KUNDEN_NOTIZ { get; set; }
-
-			public string KENNZ_VH { get; set; }
-
-			public string ALT_KENNZ { get; set; }
-
-			public string ZBII_ALT_NEU { get; set; }
-
-			public string VH_KENNZ_RES { get; set; }
-
-			public DateTime? STILL_DAT { get; set; }
-
 			public string ERROR_TEXT { get; set; }
 
 			public string PRALI_PRINT { get; set; }
 
-			public string UMBU { get; set; }
-
-			public string ABGESAGT { get; set; }
-
-			public string RUECKBU { get; set; }
-
-			public string ZZEVB { get; set; }
-
 			public string FLIEGER { get; set; }
-
-			public string OBJECT_ID { get; set; }
-
-			public string FAHRZ_ART { get; set; }
-
-			public string MNRESW { get; set; }
-
-			public string SERIE { get; set; }
-
-			public string SAISON_KNZ { get; set; }
-
-			public string SAISON_BEG { get; set; }
-
-			public string SAISON_END { get; set; }
-
-			public string TUEV_AU { get; set; }
-
-			public string KURZZEITVS { get; set; }
-
-			public string ZOLLVERS { get; set; }
-
-			public string ZOLLVERS_DAUER { get; set; }
-
-			public string VORFUEHR { get; set; }
-
-			public DateTime? HALTE_DAUER { get; set; }
-
-			public string LTEXT_NR { get; set; }
 
 			public string BEB_STATUS { get; set; }
 
-			public string AH_DOKNAME { get; set; }
-
-			public string ABM_STATUS { get; set; }
-
-			public string O_G_VERSSCHEIN { get; set; }
-
-			public string MOBUSER { get; set; }
-
 			public string INFO_TEXT { get; set; }
+
+			public string ZL_RL_FRBNR_HIN { get; set; }
+
+			public string ZL_RL_FRBNR_ZUR { get; set; }
 
 			public string NACHBEARBEITEN { get; set; }
 
-			public string VZ_UMBU_BELNR { get; set; }
+			public string LTEXT_NR { get; set; }
 
-			public string ONLINE_VG { get; set; }
+			public string MOBUSER { get; set; }
 
-			public string UMBU_GEB_FIL { get; set; }
+			public string ZZEVB { get; set; }
 
-			public string BEGRUENDUNG { get; set; }
-
-			public string STORNOGRUND { get; set; }
-
-			public string ZULBELN_NEU { get; set; }
-
-			public string ZULBELN_ALT { get; set; }
-
-			public string QMNUM { get; set; }
-
-			public string SOFORT_ABR_ERL { get; set; }
-
-			public string SA_PFAD { get; set; }
-
-			public string BRIEFNR { get; set; }
-
-			public string ORDERID { get; set; }
-
-			public string HPPOS { get; set; }
+			public string ZAHLART { get; set; }
 
 			public static ES_BAK Create(DataRow row, ISapConnection sapConnection = null, IDynSapProxyFactory dynSapProxyFactory = null)
 			{
 				var o = new ES_BAK
 				{
-					MANDT = (string)row["MANDT"],
 					ZULBELN = (string)row["ZULBELN"],
 					VBELN = (string)row["VBELN"],
 					VKORG = (string)row["VKORG"],
 					VKBUR = (string)row["VKBUR"],
 					VE_ERDAT = (string.IsNullOrEmpty(row["VE_ERDAT"].ToString())) ? null : (DateTime?)row["VE_ERDAT"],
-					VE_ERZEIT = (string)row["VE_ERZEIT"],
 					VE_ERNAM = (string)row["VE_ERNAM"],
+					VE_ERZEIT = (string)row["VE_ERZEIT"],
 					ERDAT = (string.IsNullOrEmpty(row["ERDAT"].ToString())) ? null : (DateTime?)row["ERDAT"],
 					ERNAM = (string)row["ERNAM"],
-					FLAG = (string)row["FLAG"],
 					STATUS = (string)row["STATUS"],
 					BLTYP = (string)row["BLTYP"],
 					VZB_STATUS = (string)row["VZB_STATUS"],
@@ -265,94 +147,38 @@ namespace SapORM.Models
 					VZERDAT = (string.IsNullOrEmpty(row["VZERDAT"].ToString())) ? null : (DateTime?)row["VZERDAT"],
 					BARCODE = (string)row["BARCODE"],
 					KUNNR = (string)row["KUNNR"],
-					KUNNR_NEU = (string)row["KUNNR_NEU"],
 					ZZREFNR1 = (string)row["ZZREFNR1"],
 					ZZREFNR2 = (string)row["ZZREFNR2"],
-					ZZREFNR3 = (string)row["ZZREFNR3"],
-					ZZREFNR4 = (string)row["ZZREFNR4"],
-					ZZREFNR5 = (string)row["ZZREFNR5"],
-					ZZREFNR6 = (string)row["ZZREFNR6"],
-					ZZREFNR7 = (string)row["ZZREFNR7"],
-					ZZREFNR8 = (string)row["ZZREFNR8"],
-					ZZREFNR9 = (string)row["ZZREFNR9"],
-					ZZREFNR10 = (string)row["ZZREFNR10"],
 					KREISKZ = (string)row["KREISKZ"],
 					KREISBEZ = (string)row["KREISBEZ"],
 					WUNSCHKENN_JN = (string)row["WUNSCHKENN_JN"],
 					RESERVKENN_JN = (string)row["RESERVKENN_JN"],
 					RESERVKENN = (string)row["RESERVKENN"],
-					FEINSTAUBAMT = (string)row["FEINSTAUBAMT"],
 					ZZZLDAT = (string.IsNullOrEmpty(row["ZZZLDAT"].ToString())) ? null : (DateTime?)row["ZZZLDAT"],
 					ZZKENN = (string)row["ZZKENN"],
-					WU_KENNZ2 = (string)row["WU_KENNZ2"],
-					WU_KENNZ3 = (string)row["WU_KENNZ3"],
-					KENNZTYP = (string)row["KENNZTYP"],
 					KENNZFORM = (string)row["KENNZFORM"],
 					KENNZANZ = (string)row["KENNZANZ"],
 					EINKENN_JN = (string)row["EINKENN_JN"],
-					ZUSKENNZ = (string)row["ZUSKENNZ"],
 					BEMERKUNG = (string)row["BEMERKUNG"],
 					EC_JN = (string)row["EC_JN"],
 					BAR_JN = (string)row["BAR_JN"],
 					RE_JN = (string)row["RE_JN"],
-					POOLNR = (string)row["POOLNR"],
-					ZL_RL_FRBNR_HIN = (string)row["ZL_RL_FRBNR_HIN"],
-					ZL_RL_FRBNR_ZUR = (string)row["ZL_RL_FRBNR_ZUR"],
 					ZL_LIFNR = (string)row["ZL_LIFNR"],
 					KUNDEBAR_JN = (string)row["KUNDEBAR_JN"],
-					ZAHLART = (string)row["ZAHLART"],
 					LOEKZ = (string)row["LOEKZ"],
 					KSTATUS = (string)row["KSTATUS"],
-					BARQ_NR = (string)row["BARQ_NR"],
-					VK_KUERZEL = (string)row["VK_KUERZEL"],
-					KUNDEN_REF = (string)row["KUNDEN_REF"],
-					KUNDEN_NOTIZ = (string)row["KUNDEN_NOTIZ"],
-					KENNZ_VH = (string)row["KENNZ_VH"],
-					ALT_KENNZ = (string)row["ALT_KENNZ"],
-					ZBII_ALT_NEU = (string)row["ZBII_ALT_NEU"],
-					VH_KENNZ_RES = (string)row["VH_KENNZ_RES"],
-					STILL_DAT = (string.IsNullOrEmpty(row["STILL_DAT"].ToString())) ? null : (DateTime?)row["STILL_DAT"],
 					ERROR_TEXT = (string)row["ERROR_TEXT"],
 					PRALI_PRINT = (string)row["PRALI_PRINT"],
-					UMBU = (string)row["UMBU"],
-					ABGESAGT = (string)row["ABGESAGT"],
-					RUECKBU = (string)row["RUECKBU"],
-					ZZEVB = (string)row["ZZEVB"],
 					FLIEGER = (string)row["FLIEGER"],
-					OBJECT_ID = (string)row["OBJECT_ID"],
-					FAHRZ_ART = (string)row["FAHRZ_ART"],
-					MNRESW = (string)row["MNRESW"],
-					SERIE = (string)row["SERIE"],
-					SAISON_KNZ = (string)row["SAISON_KNZ"],
-					SAISON_BEG = (string)row["SAISON_BEG"],
-					SAISON_END = (string)row["SAISON_END"],
-					TUEV_AU = (string)row["TUEV_AU"],
-					KURZZEITVS = (string)row["KURZZEITVS"],
-					ZOLLVERS = (string)row["ZOLLVERS"],
-					ZOLLVERS_DAUER = (string)row["ZOLLVERS_DAUER"],
-					VORFUEHR = (string)row["VORFUEHR"],
-					HALTE_DAUER = (string.IsNullOrEmpty(row["HALTE_DAUER"].ToString())) ? null : (DateTime?)row["HALTE_DAUER"],
-					LTEXT_NR = (string)row["LTEXT_NR"],
 					BEB_STATUS = (string)row["BEB_STATUS"],
-					AH_DOKNAME = (string)row["AH_DOKNAME"],
-					ABM_STATUS = (string)row["ABM_STATUS"],
-					O_G_VERSSCHEIN = (string)row["O_G_VERSSCHEIN"],
-					MOBUSER = (string)row["MOBUSER"],
 					INFO_TEXT = (string)row["INFO_TEXT"],
+					ZL_RL_FRBNR_HIN = (string)row["ZL_RL_FRBNR_HIN"],
+					ZL_RL_FRBNR_ZUR = (string)row["ZL_RL_FRBNR_ZUR"],
 					NACHBEARBEITEN = (string)row["NACHBEARBEITEN"],
-					VZ_UMBU_BELNR = (string)row["VZ_UMBU_BELNR"],
-					ONLINE_VG = (string)row["ONLINE_VG"],
-					UMBU_GEB_FIL = (string)row["UMBU_GEB_FIL"],
-					BEGRUENDUNG = (string)row["BEGRUENDUNG"],
-					STORNOGRUND = (string)row["STORNOGRUND"],
-					ZULBELN_NEU = (string)row["ZULBELN_NEU"],
-					ZULBELN_ALT = (string)row["ZULBELN_ALT"],
-					QMNUM = (string)row["QMNUM"],
-					SOFORT_ABR_ERL = (string)row["SOFORT_ABR_ERL"],
-					SA_PFAD = (string)row["SA_PFAD"],
-					BRIEFNR = (string)row["BRIEFNR"],
-					ORDERID = (string)row["ORDERID"],
-					HPPOS = (string)row["HPPOS"],
+					LTEXT_NR = (string)row["LTEXT_NR"],
+					MOBUSER = (string)row["MOBUSER"],
+					ZZEVB = (string)row["ZZEVB"],
+					ZAHLART = (string)row["ZAHLART"],
 
 					SAPConnection = sapConnection,
 					DynSapProxyFactory = dynSapProxyFactory,
@@ -460,8 +286,6 @@ namespace SapORM.Models
 			[ScriptIgnore]
 			public IDynSapProxyFactory DynSapProxyFactory { get; set; }
 
-			public string MANDT { get; set; }
-
 			public string ZULBELN { get; set; }
 
 			public string BANKL { get; set; }
@@ -486,7 +310,6 @@ namespace SapORM.Models
 			{
 				var o = new ES_BANK
 				{
-					MANDT = (string)row["MANDT"],
 					ZULBELN = (string)row["ZULBELN"],
 					BANKL = (string)row["BANKL"],
 					BANKN = (string)row["BANKN"],
