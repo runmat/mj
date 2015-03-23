8329f7e3c22c386adc0f09ebde33269c4448b596
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-03-23 11:29:07 +0100

    DateRange extensions

--- Work/Tools/GeneralTools/Models/DateRange.cs
+++ Work/Tools/GeneralTools/Models/DateRange.cs
@@ -7,11 +7,79 @@ namespace GeneralTools.Models
 
     public class DateRange : INullable
     {
+        private DateRangeType _rangeType;
         public bool IsSelected { get; set; }
 
         public DateTime? StartDate { get; set; }
 
         public DateTime? EndDate { get; set; }
+
+        public DateRangeType RangeType
+        {
+            get { return _rangeType; }
+            set
+            {
+                _rangeType = value;
+
+                switch (_rangeType)
+                {
+                    case DateRangeType.LastYear:
+                        StartDate = new DateTime(DateTime.Today.Year - 1, 1, 1);
+                        EndDate = new DateTime(DateTime.Today.Year - 1, 12, 31);
+                        break;
+
+                    case DateRangeType.Last3Months:
+                        StartDate = DateTime.Today.AddMonths(-3).MoveToFirstDay();
+                        EndDate = DateTime.Today.AddMonths(-1).MoveToLastDay();
+                        break;
+
+                    case DateRangeType.LastMonth:
+                        StartDate = DateTime.Today.AddMonths(-1).MoveToFirstDay();
+                        EndDate = DateTime.Today.AddMonths(-1).MoveToLastDay();
+                        break;
+
+                    case DateRangeType.CurrentMonth:
+                        StartDate = DateTime.Today.AddMonths(0).MoveToFirstDay();
+                        EndDate = DateTime.Today.AddMonths(0).MoveToLastDay();
+                        break;
+
+                    case DateRangeType.Last6Months:
+                        StartDate = DateTime.Today.AddMonths(-6);
+                        EndDate = DateTime.Today;
+                        break;
+
+                    case DateRangeType.Last90Days:
+                        StartDate = DateTime.Today.AddDays(-90);
+                        EndDate = DateTime.Today;
+                        break;
+
+                    case DateRangeType.Last60Days:
+                        StartDate = DateTime.Today.AddDays(-60);
+                        EndDate = DateTime.Today;
+                        break;
+
+                    case DateRangeType.Last30Days:
+                        StartDate = DateTime.Today.AddDays(-30);
+                        EndDate = DateTime.Today;
+                        break;
+
+                    case DateRangeType.Last7Days:
+                        StartDate = DateTime.Today.AddDays(-7);
+                        EndDate = DateTime.Today.AddDays(0);
+                        break;
+
+                    case DateRangeType.Today:
+                        StartDate = DateTime.Today;
+                        EndDate = DateTime.Today;
+                        break;
+
+                    case DateRangeType.Yesterday:
+                        StartDate = DateTime.Today.AddDays(-1);
+                        EndDate = DateTime.Today.AddDays(-1);
+                        break;
+                }
+            }
+        }
 
         public DateRange()
         {
@@ -19,65 +87,8 @@ namespace GeneralTools.Models
 
         public DateRange(DateRangeType dateRangeType, bool isSelected = false)
         {
+            RangeType = dateRangeType;
             IsSelected = isSelected;
-
-            switch (dateRangeType)
-            {
-                case DateRangeType.LastYear:
-                    StartDate = new DateTime(DateTime.Today.Year - 1, 1, 1);
-                    EndDate = new DateTime(DateTime.Today.Year - 1, 12, 31);
-                    break;
-
-                case DateRangeType.Last3Months:
-                    StartDate = DateTime.Today.AddMonths(-3).MoveToFirstDay();
-                    EndDate = DateTime.Today.AddMonths(-1).MoveToLastDay();
-                    break;
-
-                case DateRangeType.LastMonth:
-                    StartDate = DateTime.Today.AddMonths(-1).MoveToFirstDay();
-                    EndDate = DateTime.Today.AddMonths(-1).MoveToLastDay();
-                    break;
-
-                case DateRangeType.CurrentMonth:
-                    StartDate = DateTime.Today.AddMonths(0).MoveToFirstDay();
-                    EndDate = DateTime.Today.AddMonths(0).MoveToLastDay();
-                    break;
-
-                case DateRangeType.Last6Months:
-                    StartDate = DateTime.Today.AddMonths(-6);
-                    EndDate = DateTime.Today;
-                    break;
-
-                case DateRangeType.Last90Days:
-                    StartDate = DateTime.Today.AddDays(-90);
-                    EndDate = DateTime.Today;
-                    break;
-
-                case DateRangeType.Last60Days:
-                    StartDate = DateTime.Today.AddDays(-60);
-                    EndDate = DateTime.Today;
-                    break;
-
-                case DateRangeType.Last30Days:
-                    StartDate = DateTime.Today.AddDays(-30);
-                    EndDate = DateTime.Today;
-                    break;
-
-                case DateRangeType.Last7Days:
-                    StartDate = DateTime.Today.AddDays(-7);
-                    EndDate = DateTime.Today.AddDays(0);
-                    break;
-
-                case DateRangeType.Today:
-                    StartDate = DateTime.Today;
-                    EndDate = DateTime.Today;
-                    break;
-
-                case DateRangeType.Yesterday:
-                    StartDate = DateTime.Today.AddDays(-1);
-                    EndDate = DateTime.Today.AddDays(-1);
-                    break;
-            }
         }
 
         public bool IsNull()
