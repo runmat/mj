761be6e3e1ebb50631b391d56a9ac268db49484f
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-05-21 17:44:57 +0200

    first draft

---
+++ Work/CkgDomainLogic/CkgDomainEqui/Fahrzeuge/ViewModels/ZulassungViewModel.cs
@@ -0,0 +1,76 @@
+﻿// ReSharper disable RedundantUsingDirective
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Xml.Serialization;
+using CkgDomainLogic.General.Models;
+using CkgDomainLogic.General.Services;
+using CkgDomainLogic.General.ViewModels;
+using System.Web.Mvc;
+using CkgDomainLogic.Fahrzeuge.Contracts;
+using CkgDomainLogic.Fahrzeuge.Models;
+using GeneralTools.Models;
+using System.IO;
+using GeneralTools.Services;
+// ReSharper restore RedundantUsingDirective
+
+namespace CkgDomainLogic.Fahrzeuge.ViewModels
+{
+    public class ZulassungViewModel : CkgBaseViewModel
+    {
+        [XmlIgnore]
+        public IFahrzeugeDataService DataService { get { return CacheGet<IFahrzeugeDataService>(); } }
+
+        [XmlIgnore]
+        public List<AbgemeldetesFahrzeug> Fahrzeuge
+        {
+            get { return PropertyCacheGet(() => new List<AbgemeldetesFahrzeug>
+            {
+                new AbgemeldetesFahrzeug { Briefnummer = "4711" },
+                new AbgemeldetesFahrzeug { Briefnummer = "4712" },
+            });
+            }
+            protected set { PropertyCacheSet(value); }
+        }
+
+        [XmlIgnore]
+        public List<AbgemeldetesFahrzeug> FahrzeugeFiltered
+        {
+            get { return PropertyCacheGet(() => Fahrzeuge); }
+            protected set { PropertyCacheSet(value); }
+        }
+
+        [XmlIgnore]
+        public List<FahrzeugStatus> FahrzeugStatusWerte
+        {
+            get { return PropertyCacheGet(() => DataService.FahrzeugStatusWerte); }
+        }
+
+
+        public void DataInit()
+        {
+            AbgemeldeteFahrzeugeSelektor.AlleFahrzeugStatusWerteStatic = DataService.FahrzeugStatusWerte;
+            DataMarkForRefresh();
+        }
+
+        public void DataInit(bool preSelection)
+        {
+            DataInit();
+        }
+
+        public void DataMarkForRefresh()
+        {
+            PropertyCacheClear(this, m => m.FahrzeugeFiltered);
+        }
+
+        public void Validate(Action<Expression<Func<AbgemeldeteFahrzeugeSelektor, object>>, string> addModelError)
+        {
+        }
+
+        public void FilterFahrzeuge(string filterValue, string filterProperties)
+        {
+            FahrzeugeFiltered = Fahrzeuge.SearchPropertiesWithOrCondition(filterValue, filterProperties);
+        }
+    }
+}
---
+++ inetpub/wwwroot/ServicesMvc/Areas/Fahrzeug/Controllers/ZulassungController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using CkgDomainLogic.General.Contracts;
+using CkgDomainLogic.General.Controllers;
+using CkgDomainLogic.Fahrzeuge.Contracts;
+using CkgDomainLogic.Fahrzeuge.ViewModels;
+using GeneralTools.Contracts;
+
+namespace ServicesMvc.Fahrzeug.Controllers
+{
+    public class ZulassungController : CkgDomainController 
+    {
+        public override string DataContextKey { get { return GetDataContextKey<ZulassungViewModel>(); } }
+
+        public ZulassungViewModel ViewModel 
+        { 
+            get { return GetViewModel<ZulassungViewModel>(); } 
+            set { SetViewModel(value); } 
+        }
+
+        public ZulassungController(IAppSettings appSettings, ILogonContextDataService logonContext,
+            IFahrzeugeDataService fahrzeugeDataService)
+            : base(appSettings, logonContext)
+        {
+            InitViewModel(ViewModel, appSettings, logonContext, fahrzeugeDataService);
+            InitModelStatics();
+        }
+
+        [CkgApplication]
+        public ActionResult Index()
+        {
+            return View(ViewModel);
+        }
+
+        static void InitModelStatics()
+        {
+        }
+    }
+}
