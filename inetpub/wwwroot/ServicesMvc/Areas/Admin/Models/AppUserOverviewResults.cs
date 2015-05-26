c4e84ce0a527cc7826b01e718a4f524d6a7ef5b9
unknown <MaihoferM@AHW570.Kroschke.de>
2015-05-26 14:14:58 +0200

    ITA 8066 Implementation with new MVC Area "Admin"

---
+++ inetpub/wwwroot/ServicesMvc/Areas/Admin/Models/AppUserOverviewResults.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GeneralTools.Models;
+
+namespace ServicesMvc.AppUserOverview.Models
+{
+    public class AppUserOverviewResults
+    {
+
+        [LocalizedDisplay("Anwendungsname 1")]      //[LocalizedDisplay(LocalizeConstants.ContractNo)]
+        public string AppName { get; set; }
+
+        [LocalizedDisplay("Anwendungsname 2")]
+        public string AppFriendlyName { get; set; }
+
+        [LocalizedDisplay("Kunde")]
+        public string Customername { get; set; }
+
+        [LocalizedDisplay("Gruppe")]
+        public string GroupName { get; set; }
+
+        [LocalizedDisplay("Benutzeranzahl")]
+        public int WebUserCount { get; set; }
+
+        [LocalizedDisplay("Benutzer aktiv")]
+        public string HasActiveWebUsers { get; set; }
+
+    }
+}
---
+++ inetpub/wwwroot/ServicesMvc/Areas/Admin/Models/AppUserOverviewSelektor.cs
@@ -0,0 +1,30 @@
+﻿using CkgDomainLogic.General.Services;
+using GeneralTools.Models;
+using GeneralTools.Resources;
+using GeneralTools.Services;
+
+namespace CkgDomainLogic.AppUserOverview.Models
+{
+    public class AppUserOverviewSelektor : Store
+    {
+        /// <summary>
+        /// Default value = Nur aktive anzeigen ("Active")
+        /// </summary>
+        [LocalizedDisplay("Nur aktive")]    //[LocalizedDisplay(LocalizeConstants.VIN17)]
+        public string ShowActiveOnly { get { return PropertyCacheGet(() => "Active"); } set { PropertyCacheSet(value); } }  
+
+        /// <summary>
+        /// Inhalte der Radiobuttons für Auswahl bereitstellen
+        /// </summary>
+        public string ShowActiveOnlyWerte
+        {
+            get
+            {
+                return string.Format("{0},{1};{2},{3}",
+                    "Active", "Ja",    // Localize.VehiclesAll,
+                    "All", "Nein");    // Localize.VehiclesOnlyWithoutFirstRegistration);
+            }
+        }
+
+    }
+}
