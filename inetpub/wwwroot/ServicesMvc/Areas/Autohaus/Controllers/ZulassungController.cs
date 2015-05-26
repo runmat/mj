120fcf573553fea354f1702a8eed3ebf3fe34c49
unknown <MaihoferM@AHW570.Kroschke.de>
2015-05-26 17:17:43 +0200

    8071 Zulassung: Download all PDF files

--- inetpub/wwwroot/ServicesMvc/Areas/Autohaus/Controllers/ZulassungController.cs
+++ inetpub/wwwroot/ServicesMvc/Areas/Autohaus/Controllers/ZulassungController.cs
@@ -380,11 +380,50 @@ namespace ServicesMvc.Autohaus.Controllers
         /// <returns></returns>
         public FileContentResult AllDocumentsAsPdf(string id, string typ)
         {
-            //var zulassung = ViewModel.ZulassungenForReceipt.FirstOrDefault(z => z.BelegNr == id);
-            //if (zulassung == null)
-            //    return new FileContentResult(new byte[1], "");
-            //var formularPdfBytes = zulassung.KundenformularPdf;
-            //return new FileContentResult(formularPdfBytes, "application/pdf") { FileDownloadName = String.Format("{0}.pdf", Localize.CustomerForm) };
+            var foo = PdfDocumentFactory.HtmlToPdf("test");
+
+            // Merge multiple PDF files into one single PDF 
+            // http://stackoverflow.com/questions/6029142/merging-multiple-pdfs-using-itextsharp-in-c-net
+
+            // step 1: creation of a document-object
+            iTextSharp document = new Document();
+
+            // step 2: we create a writer that listens to the document
+            PdfCopy writer = new PdfCopy(document, new FileStream(outFile, FileMode.Create));
+            if (writer == null)
+            {
+                return;
+            }
+
+            // step 3: we open the document
+            document.Open();
+
+            foreach (string fileName in fileNames)
+            {
+                // we create a reader for a certain document
+                PdfReader reader = new PdfReader(fileName);
+                reader.ConsolidateNamedDestinations();
+
+                // step 4: we add content
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    PdfImportedPage page = writer.GetImportedPage(reader, i);
+                    writer.AddPage(page);
+                }
+
+                PRAcroForm form = reader.AcroForm;
+                if (form != null)
+                {
+                    writer.CopyAcroForm(reader);
+                }
+
+                reader.Close();
+            }
+
+            // step 5: we close the document and writer
+            writer.Close();
+            document.Close();
+
 
             return null;
         }
