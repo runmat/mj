fa0df4cd816a7f665b620aa9c8a249fb6a727234
unknown <MaihoferM@AHW570.Kroschke.de>
2015-05-28 15:25:46 +0200

    Implementation

--- Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/Models/Zulassungsdaten.cs
+++ Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/Models/Zulassungsdaten.cs
@@ -39,6 +39,11 @@ namespace CkgDomainLogic.Autohaus.Models
             }
         }
 
+        // 20150528 MMA 
+        [LocalizedDisplay("Mindesthaltedauer")]
+        [Range(1 , 360, ErrorMessage = "Nur zwischen 1 und 360")]
+        public string Mindesthaltedauer { get; set; }
+
         [XmlIgnore]
         static List<Material> MaterialList { get { return GetZulassungViewModel == null ? new List<Material>() : GetZulassungViewModel().Zulassungsarten; } }
 
