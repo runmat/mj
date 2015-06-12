aeac7e9dca7c1d8c134da7ffcbc3d70c6ef34e21
unknown <MaihoferM@AHW570.Kroschke.de>
2015-06-12 15:00:28 +0200

    Implementierung

---
+++ inetpub/wwwroot/ServicesMvc/Areas/Autohaus/Models/PartnerAdresse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CkgDomainLogic.DomainCommon.Models;
+
+namespace ServicesMvc.Areas.Autohaus.Models
+{
+    public class PartnerAdresse : Adresse
+    {
+    }
+}
