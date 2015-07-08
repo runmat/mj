bd9379d8decea115a4160eba03fa758006340766
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-07-08 09:01:02 +0200

    second draft

--- Work/Tools/MvcTools/Web/WebFormViewEngineThemed.cs
+++ Work/Tools/MvcTools/Web/WebFormViewEngineThemed.cs
@@ -13,12 +13,20 @@ namespace MvcTools.Web
             var logonContext = (ILogonContext)SessionHelper.GetSessionObject("LogonContext");
             if (logonContext != null && logonContext.KundenNr.NotNullOrEmpty().Trim('0') == "240145")
             {
-                if (partialViewName.ToLower().Contains("usermenu") || partialViewName.ToLower().Contains("formsearchbox"))
+                if (partialViewName.ToLower().Contains("usermenu"))
+                {
+                    var x = partialViewName;
+                }
+                if (partialViewName.ToLower().Contains("formsearchbox"))
+                {
+                    var x = partialViewName;
+                }
+                if (partialViewName.ToLower().Contains("partial/suche"))
                 {
                     var x = partialViewName;
                 }
 
-                result = base.FindPartialView(controllerContext, "Formulare/" + partialViewName, useCache);
+                result = base.FindPartialView(controllerContext, "_Themes/AH2015/" + partialViewName, useCache);
             }
 
             //Fall back to default search path if no other view has been selected  
