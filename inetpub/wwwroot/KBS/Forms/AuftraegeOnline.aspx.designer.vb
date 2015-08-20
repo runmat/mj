931e5424555c08f7aaada7b0ca9909f4a5d09094
unknown <CarlM@AHW461.Kroschke.de>
2015-08-20 10:31:49 +0200

    Neue Anwendung "Aufträge Online"

---
+++ inetpub/wwwroot/KBS/Forms/AuftraegeOnline.aspx.designer.vb
@@ -0,0 +1,114 @@
+﻿'------------------------------------------------------------------------------
+' <automatisch generiert>
+'     Der Code wurde von einem Tool generiert.
+'
+'     Änderungen an der Datei führen möglicherweise zu falschem Verhalten, und sie gehen verloren, wenn
+'     der Code erneut generiert wird. 
+' </automatisch generiert>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class AuftraegeOnline
+
+    '''<summary>
+    '''lb_zurueck-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents lb_zurueck As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''lblHead-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents lblHead As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''lblPageTitle-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents lblPageTitle As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''TableQuery-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents TableQuery As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+    '''<summary>
+    '''lblError-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents lblError As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Literal1-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents Literal1 As Global.System.Web.UI.WebControls.Literal
+
+    '''<summary>
+    '''Auftragsliste-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents Auftragsliste As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+    '''<summary>
+    '''rgGrid1-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents rgGrid1 As Global.Telerik.Web.UI.RadGrid
+
+    '''<summary>
+    '''lbAuswahlAlle-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents lbAuswahlAlle As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''lbAlleDokumente-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents lbAlleDokumente As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''lbAbsenden-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+    '''</remarks>
+    Protected WithEvents lbAbsenden As Global.System.Web.UI.WebControls.LinkButton
+End Class
---
+++ inetpub/wwwroot/KBS/Forms/AuftraegeOnline.aspx.vb
@@ -0,0 +1,129 @@
+﻿Imports System
+Imports KBS.KBS_BASE
+Imports Telerik.Web.UI
+
+Public Class AuftraegeOnline
+    Inherits Page
+
+    Private mObjKasse As Kasse
+    Private mObjOnline As Online
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        FormAuth(Me)
+        If Not Session("mKasse") Is Nothing Then
+            mObjKasse = Session("mKasse")
+        End If
+
+        lblError.Text = ""
+        Literal1.Text = ""
+        Title = lblHead.Text
+
+        If Session("ObjOnline") IsNot Nothing Then
+            mObjOnline = CType(Session("ObjOnline"), Online)
+        Else
+            mObjOnline = New Online(mObjKasse.Lagerort)
+            Session("ObjOnline") = mObjOnline
+        End If
+
+        If Not IsPostBack Then
+            Session("LastPage") = Me
+
+            LadeAuftraege()
+        End If
+    End Sub
+
+    Private Sub LadeAuftraege()
+        mObjOnline.LoadAuftraege()
+        Session("ObjOnline") = mObjOnline
+
+        If Not mObjOnline.ErrorOccured Then
+            FillGrid()
+        Else
+            lblError.Text = "Fehler beim Laden der Auftragsliste: " & mObjOnline.ErrorMessage
+        End If
+    End Sub
+
+    Private Sub FillGrid()
+        If mObjOnline.Auftraege IsNot Nothing AndAlso mObjOnline.Auftraege.Rows.Count > 0 Then
+            rgGrid1.Visible = True
+            rgGrid1.Rebind()
+            'Setzen der DataSource geschieht durch das NeedDataSource-Event
+        Else
+            rgGrid1.Visible = False
+            lblError.Text = "Keine Daten gefunden"
+        End If
+    End Sub
+
+    Protected Sub rgGrid1_NeedDataSource(ByVal sender As Object, ByVal e As GridNeedDataSourceEventArgs) Handles rgGrid1.NeedDataSource
+        If mObjOnline.Auftraege IsNot Nothing Then
+            rgGrid1.DataSource = mObjOnline.Auftraege.DefaultView
+        Else
+            rgGrid1.DataSource = Nothing
+        End If
+    End Sub
+
+    Protected Sub lbAuswahlAlle_Click(ByVal sender As Object, ByVal e As EventArgs) Handles lbAuswahlAlle.Click
+        mObjOnline.SetAuswahlAlle()
+        Session("ObjOnline") = mObjOnline
+        FillGrid()
+    End Sub
+
+    Protected Sub chkAuswahl_OnCheckedChanged(ByVal sender As Object, ByVal e As EventArgs)
+        Dim cbx As CheckBox = CType(sender, CheckBox)
+        Dim item As GridDataItem = CType(cbx.NamingContainer, GridDataItem)
+        Dim praegId As String = item.GetDataKeyValue("PRAEG_ID").ToString()
+
+        mObjOnline.SetAuswahlAuftrag(praegId, cbx.Checked)
+        Session("ObjOnline") = mObjOnline
+    End Sub
+
+    Protected Sub rgGrid1_ItemCommand(ByVal sender As Object, ByVal e As GridCommandEventArgs) Handles rgGrid1.ItemCommand
+        If TypeOf e.Item Is GridDataItem Then
+            Dim gridRow As GridDataItem = CType(e.Item, GridDataItem)
+
+            If e.CommandName = "showDocument" Then
+                ShowPdf(gridRow("PRAEG_ID").Text)
+            End If
+        End If
+    End Sub
+
+    Protected Sub lbAlleDokumente_Click(ByVal sender As Object, ByVal e As EventArgs) Handles lbAlleDokumente.Click
+        ShowPdf()
+    End Sub
+
+    Private Sub ShowPdf(Optional ByVal praegId As String = Nothing)
+        Dim pdfBytes As Byte() = mObjOnline.GetMergedPdf(praegId)
+
+        If mObjOnline.ErrorOccured OrElse pdfBytes Is Nothing Then
+            lblError.Text = "Beim Abrufen des Dokuments ist ein Fehler aufgetreten: " & mObjOnline.ErrorMessage
+            Exit Sub
+        End If
+
+        Session("OnlinePdfBytes") = pdfBytes
+
+        Literal1.Text = "						<script language=""Javascript"">" & Environment.NewLine
+        Literal1.Text &= "						  <!-- //" & Environment.NewLine
+        Literal1.Text &= "                          window.open(""DownloadFile2.aspx"", ""_blank"", ""left=0,top=0,resizable=YES,scrollbars=YES"");" & Environment.NewLine
+        Literal1.Text &= "						  //-->" & Environment.NewLine
+        Literal1.Text &= "						</script>" & Environment.NewLine
+    End Sub
+
+    Protected Sub lb_zurueck_Click(sender As Object, e As EventArgs) Handles lb_zurueck.Click
+        Session("ObjOnline") = Nothing
+        Session("OnlinePdfBytes") = Nothing
+        Response.Redirect("../Selection.aspx")
+    End Sub
+
+    Protected Sub lbAbsenden_Click(ByVal sender As Object, ByVal e As EventArgs) Handles lbAbsenden.Click
+        mObjOnline.SendAuftraege()
+
+        If Not mObjOnline.ErrorOccured Then
+            lblError.Text = "Aufträge erfolgreich gespeichert"
+            Session("mObjOnline") = mObjOnline
+            FillGrid()
+        Else
+            lblError.Text = "Fehler beim Speichern der Aufträge: " & mObjOnline.ErrorMessage
+        End If
+    End Sub
+
+End Class
