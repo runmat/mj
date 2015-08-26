3490a8b66adcd78b6b08d3b1f65b259add81e89a
Marco Carl <marco.carl@kroschke.de>
2015-08-26 08:04:28 +0200

    Fix für SapORM nach ErpConnect-Umstellung: Verwendung von Erweiterungsmethode ToAdoTableLocaleDe statt ToAdoTable

---
+++ Work/SapORM/DynSapProxy/ProxyObject/DynSapProxyObjectErp35/ErpExtensions.cs
@@ -0,0 +1,16 @@
+﻿﻿using System.Data;
+using System.Globalization;
+using ERPConnect;
+
+namespace SapORM.Services
+{
+    public static class ErpExtensions
+    {
+        public static DataTable ToADOTableLocaleDe(this RFCTable rfct)
+        {
+            var tmpTable = rfct.ToADOTable();
+            tmpTable.Locale = new CultureInfo("de-DE");
+            return tmpTable;
+        }
+    }
+}
