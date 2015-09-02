2bcedd7ee89b3df306de8d052cd8669be159bba6
Marco Carl <marco.carl@kroschke.de>
2015-09-02 15:42:38 +0200

    Reservierungs-Name und Halterdauer in Tagen mitspeichern (inkl. Warenkorb)

--- Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/Models/AppModelMappings.cs
+++ Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/Models/AppModelMappings.cs
@@ -320,6 +320,8 @@ namespace CkgDomainLogic.Autohaus.Models
                         d.Zulassungsdaten.VorhandenesKennzeichenReservieren = s.VH_KENNZ_RES.XToBool();
                         d.Zulassungsdaten.KennzeichenReserviert = s.RESERVKENN_JN.XToBool();
                         d.Zulassungsdaten.ReservierungsNr = s.RESERVKENN;
+                        d.Zulassungsdaten.ReservierungsName = s.RES_NAME;
+                        d.Zulassungsdaten.MindesthaltedauerDays = s.HALTEDAUER;
 
                         d.Zulassungsdaten.Kennzeichen = s.ZZKENN;
                         d.Zulassungsdaten.Wunschkennzeichen2 = s.WU_KENNZ2;
@@ -467,8 +469,8 @@ namespace CkgDomainLogic.Autohaus.Models
                             d.RESERVKENN_JN = s.Zulassungsdaten.KennzeichenReserviert.BoolToX();
                             d.WUNSCHKENN_JN = s.Zulassungsdaten.WunschkennzeichenVorhanden.BoolToX();
                             d.RESERVKENN = s.Zulassungsdaten.ReservierungsNr;
-
                             d.RES_NAME = s.Zulassungsdaten.ReservierungsName;
+                            d.HALTEDAUER = s.Zulassungsdaten.MindesthaltedauerDays;
 
                             Func<string, string> formatKennzeichen = (kennzeichen => kennzeichen.NotNullOr(Zulassungsdaten.ZulassungsKennzeichenLinkeSeite(kennzeichen)));
 
