d45d53c60af77bc1ec7f80df9e0cfe5f2e86e51f
Marco Carl <marco.carl@kroschke.de>
2015-09-30 09:10:58 +0200

    Neue Anwendung "Datei-Download"

---
+++ Work/CkgDomainLogic/CkgDomainArchive/Archive/Models/DataModels/DateiInfo.cs
@@ -0,0 +1,11 @@
+﻿using GeneralTools.Models;
+using GeneralTools.Resources;
+
+namespace CkgDomainLogic.Archive.Models
+{
+    public class DateiInfo
+    {
+        [LocalizedDisplay(LocalizeConstants.FileName)]
+        public string DateiName { get; set; }
+    }
+}
---
+++ Work/CkgDomainLogic/CkgDomainArchive/Archive/Services/DateiDownloadDataService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CkgDomainLogic.General.Services;
+using CkgDomainLogic.Archive.Contracts;
+using CkgDomainLogic.Archive.Models;
+
+namespace CkgDomainLogic.Archive.Services
+{
+    public class DateiDownloadDataService : CkgGeneralDataService, IDateiDownloadDataService
+    {
+        public List<DateiInfo> LoadDateiInfos(string serverPfad, string suchPattern)
+        {
+            var liste = new List<DateiInfo>();
+
+            var dateien = Directory.GetFiles(serverPfad, suchPattern, SearchOption.TopDirectoryOnly);
+
+            foreach (var datei in dateien)
+            {
+                liste.Add(new DateiInfo() { DateiName = Path.GetFileName(datei) });
+            }
+            
+            return liste;
+        }
+    }
+}
