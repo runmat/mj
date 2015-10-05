827335acced2eaa2f11682010eef6ba7dad53fb5
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-10-05 11:13:31 +0200

    VS Solution persister, minor extension

--- dev/VsSolutionPersister/VsSolutionPersister/App.xaml.cs
+++ dev/VsSolutionPersister/VsSolutionPersister/App.xaml.cs
@@ -1,6 +1,21 @@
-﻿namespace VsSolutionPersister
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace VsSolutionPersister
 {
     public partial class App 
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (e.Args.Length >= 1)
+            {
+                if (e.Args[0].ToLower() == "clipboard")
+                    Clipboard.SetText("seE17igEl");
+
+                Shutdown(0);
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
