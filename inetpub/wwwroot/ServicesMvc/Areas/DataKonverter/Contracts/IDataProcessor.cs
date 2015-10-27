558e8b3f16f42483f6d2747965b57e9362a7a11e
unknown <MaihoferM@AHW570.Kroschke.de>
2015-10-27 13:07:19 +0100

    Dev

---
+++ inetpub/wwwroot/ServicesMvc/Areas/DataKonverter/Contracts/IDataProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesMvc.Areas.DataKonverter.Models
+{
+    interface IDataProcessor
+    {
+         
+    }
+
+
+    class DataProcessor : IDataProcessor
+    {
+
+    }
+
+
+}
