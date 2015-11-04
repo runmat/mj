c8504abf3b58928a195ce462b0e4e6e772a07835
Marco Carl <marco.carl@kroschke.de>
2015-11-04 16:20:34 +0100

    LIFNR mit führenden Nullen an SAP übergeben

--- Work/CkgDomainLogic/Lib/Zulassung/ZldPartner/Services/ZldPartnerZulassungenDataServiceSap.cs
+++ Work/CkgDomainLogic/Lib/Zulassung/ZldPartner/Services/ZldPartnerZulassungenDataServiceSap.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using CkgDomainLogic.General.Services;
 using CkgDomainLogic.ZldPartner.Contracts;
 using CkgDomainLogic.ZldPartner.Models;
+using GeneralTools.Models;
 using SapORM.Contracts;
 using SapORM.Models;
 using AppModelMappings = CkgDomainLogic.ZldPartner.Models.AppModelMappings;
@@ -19,7 +20,7 @@ namespace CkgDomainLogic.ZldPartner.Services
 
         public List<OffeneZulassung> LoadOffeneZulassungen()
         {
-            Z_ZLD_PP_GET_PO_01.Init(SAP, "I_LIFNR", LogonContext.User.Reference);
+            Z_ZLD_PP_GET_PO_01.Init(SAP, "I_LIFNR", LogonContext.User.Reference.NotNullOrEmpty().ToSapKunnr());
 
             return AppModelMappings.Z_ZLD_PP_GET_PO_01_GT_BESTELLUNGEN_To_OffeneZulassung.Copy(Z_ZLD_PP_GET_PO_01.GT_BESTELLUNGEN.GetExportListWithExecute(SAP)).ToList();
         }
@@ -52,7 +53,7 @@ namespace CkgDomainLogic.ZldPartner.Services
 
         public List<DurchgefuehrteZulassung> LoadDurchgefuehrteZulassungen(DurchgefuehrteZulassungenSuchparameter suchparameter)
         {
-            Z_ZLD_PP_GET_ZULASSUNGEN_01.Init(SAP, "I_LIFNR", LogonContext.User.Reference);
+            Z_ZLD_PP_GET_ZULASSUNGEN_01.Init(SAP, "I_LIFNR", LogonContext.User.Reference.NotNullOrEmpty().ToSapKunnr());
 
             if (!String.IsNullOrEmpty(suchparameter.Kunde))
                 SAP.SetImportParameter("I_KUNDE", suchparameter.Kunde);
