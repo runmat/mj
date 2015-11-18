784ab769be50fc13b5a8c33e3bc216a711733130
Marco Carl <marco.carl@kroschke.de>
2015-11-18 08:55:29 +0100

    div. Anpassungen

--- Work/CkgDomainLogic/Lib/Zulassung/MobileErfassung/ViewModels/ZulMobileErfassungViewModel.cs
+++ Work/CkgDomainLogic/Lib/Zulassung/MobileErfassung/ViewModels/ZulMobileErfassungViewModel.cs
@@ -86,59 +86,13 @@ namespace CkgDomainLogic.Zulassung.MobileErfassung.ViewModels
         /// <returns></returns>
         public List<AmtVorgaenge> GetAemterVorgaenge()
         {
-            //List<AmtVorgaenge> aemterMitVorgaengen;
-            //List<Vorgang> vorgaenge;
+            List<AmtVorgaenge> aemterMitVorgaengen;
+            List<Vorgang> vorgaenge;
 
-            //DataService.GetAemterMitVorgaengen(out aemterMitVorgaengen, out vorgaenge);
+            DataService.GetAemterMitVorgaengen(out aemterMitVorgaengen, out vorgaenge);
 
-            //ZLDMobileData.AemterMitVorgaengen = aemterMitVorgaengen;
-            //ZLDMobileData.Vorgaenge = vorgaenge;
-
-            var wunschkennz = true;
-            var reskennz = false;
-            var saisonkennz = false;
-
-            ZLDMobileData.Vorgaenge = new List<Vorgang>();
-            for (var i = 0; i < 20; i++)
-            {
-                wunschkennz = !wunschkennz;
-                reskennz = !reskennz;
-                saisonkennz = !saisonkennz;
-                var saisonVon = (i % 12) + 1;
-                var saisonBis = (saisonVon < 12 ? (saisonVon + 1) : saisonVon);
-
-                ZLDMobileData.Vorgaenge.Add(new Vorgang
-                {
-                    Id = (12345600 + i).ToString(),
-                    BlTyp = "NZ",
-                    VkOrg = "1010",
-                    VkBur = "4837",
-                    Kunnr = "471100",
-                    Kunname = "Dummykunde " + i.ToString(),
-                    Referenz1 = "Max Mustermann",
-                    Referenz2 = "WWW123456789000" + i.ToString("D2"),
-                    ZulDat = DateTime.Today,
-                    Amt = "B",
-                    Kennzeichen = "B-RD10" + i.ToString("D2"),
-                    Status = "A",
-                    Wunschkennzeichen = wunschkennz,
-                    Reserviert = reskennz,
-                    Saisonkennzeichen = saisonkennz,
-                    SaisonVon = saisonVon.ToString("D2"),
-                    SaisonBis = saisonBis.ToString("D2"),
-                    Infotext = "dgfgsgfdggsdf",
-                    Bemerkung = "rtzrtztrzrtzrt",
-                    Positionen = new List<VorgangPosition> { new VorgangPosition { KopfId = (12345600 + i).ToString(), PosNr = "10", DienstleistungId = "593", DienstleistungBez = "Neuzulassung", Gebuehr = 5, GebuehrenMaterial = "520" } }
-                });
-            }
-
-            ZLDMobileData.AemterMitVorgaengen = new List<AmtVorgaenge> { new AmtVorgaenge
-            {
-                KurzBez = "B",
-                Bezeichnung = "Berlin",
-                ZulDatText = "17.11.2015",
-                AnzVorgaenge = ZLDMobileData.Vorgaenge.Count
-            } };
+            ZLDMobileData.AemterMitVorgaengen = aemterMitVorgaengen;
+            ZLDMobileData.Vorgaenge = vorgaenge;
 
             return ZLDMobileData.AemterMitVorgaengen;
         }
