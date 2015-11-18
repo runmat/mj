b8da3655f41793d00f1033268157488a7d67954d
unknown <MaihoferM@AHW570.Kroschke.de>
2015-11-18 16:44:11 +0100

    Dev

--- inetpub/wwwroot/ServicesMvc/Areas/DataKonverter/Models/DestinationFile.cs
+++ inetpub/wwwroot/ServicesMvc/Areas/DataKonverter/Models/DestinationFile.cs
@@ -15,26 +15,11 @@ namespace ServicesMvc.Areas.DataKonverter.Models
         public string XmlRaw { get; set; }
         public XmlDocument XmlDocument { get; set; }
 
-        public class Column
-        {
-            public Guid Guid { get { return Guid.NewGuid(); } }
-            public string Caption { get; set; }
-            public DataType DataType { get; set; }
-            public bool IsUsed { get; set; }
-        }
-
-        public enum DataType
-        {
-            String = 1,
-            DateTime = 2,
-            Date = 3,
-            Double = 4,
-            Boolean = 5
-        }
-
         public DestinationFile()
         {
             Guid = Guid.NewGuid();
+
+            
         }
 
     }
