b0f4dfd4451e78bb43d8b4c0749a212dee48b8ea
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-12-03 17:13:30 +0100

    Reverse commit Test-Controls (1)

--- Work/CkgDomainLogic/CkgDomainEqui/Strafzettel/Models/StrafzettelSelektor.cs
+++ Work/CkgDomainLogic/CkgDomainEqui/Strafzettel/Models/StrafzettelSelektor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using CkgDomainLogic.Equi.Models;
-using CkgDomainLogic.General.Models;
-using CkgDomainLogic.General.Services;
+﻿using CkgDomainLogic.General.Models;
 using GeneralTools.Models;
 using GeneralTools.Resources;
 using GeneralTools.Services;
@@ -49,60 +45,5 @@ namespace CkgDomainLogic.Strafzettel.Models
         [LocalizedDisplay(LocalizeConstants.AuthorityPostcode)]
         [FormPersistable]
         public string BehoerdePlz { get; set; }
-
-
-        [LocalizedDisplay(LocalizeConstants.CustomizableStrafzettelRef1)]
-        public string Referenz1 { get; set; }
-
-        [LocalizedDisplay(LocalizeConstants.CustomizableStrafzettelRef2)]
-        public string Referenz2 { get; set; }
-
-
-        [LocalizedDisplay(LocalizeConstants.ForeignCountries)]
-        public bool IstAusland { get; set; }
-
-        [LocalizedDisplay(LocalizeConstants.InvoiceDate)]
-        public DateTime? RechnungsDatum { get; set; }
-
-
-        [LocalizedDisplay(LocalizeConstants._Standorte)]
-        public static List<Standort> AlleStandorte
-        {
-            get
-            {
-                return new List<Standort>
-                {
-                    new Standort("", ""),
-                    new Standort("1601", "Ingolstadt Neuwagen"),
-                    new Standort("1602", "Ingolstadt SD"),
-                    new Standort("1603", "Gebrauchtwagen"),
-                    new Standort("1604", "Ingolstadt MFC"),
-                    new Standort("1651", "Neckarsulm Neuwagen"),
-                };
-            }
-        }
-
-        [LocalizedDisplay(LocalizeConstants.Location)]
-        public List<string> Standorte
-        {
-            get { return PropertyCacheGet(() => new List<string> { "1602", "1604"}); }
-            set { PropertyCacheSet(value); }
-        }
-
-
-        [LocalizedDisplay(LocalizeConstants.VehiclesLicenseStatus)]
-        [FormPersistable]
-        public string FahrzeugStatus { get { return PropertyCacheGet(() => "VehiclesOnlyLicensed"); } set { PropertyCacheSet(value); } }
-
-        public static string FahrzeugStatusWerte
-        {
-            get
-            {
-                return string.Format("{0},{1};{2},{3};{4},{5}",
-                    "VehiclesAll", Localize.VehiclesAll,
-                    "VehiclesOnlyLicensed", Localize.VehiclesOnlyLicensed,
-                    "VehiclesOnlyUnlicensed", Localize.VehiclesOnlyWithoutFirstRegistration);
-            }
-        }
     }
 }
