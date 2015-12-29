0bfdb404e7550006398383c93d7cc574c763276b
Matthias Jenzen <matthias.jenzen@kroschke.de>
2015-12-29 15:18:57 +0100

    minor changes

--- inetpub/wwwroot/ServicesMvc/Areas/Common/Content/Dashboard/flotr2/flotr2.js
+++ inetpub/wwwroot/ServicesMvc/Areas/Common/Content/Dashboard/flotr2/flotr2.js
@@ -5999,7 +5999,7 @@ Flotr.addPlugin('hit', {
       oTop = offset.top;
       oLeft = offset.left;
     }
-
+      
     if (!n.mouse.relative) { // absolute to the canvas
       pos += 'top:';
       if      (p.charAt(0) == 'n') pos += (oTop + m + top);
@@ -6018,7 +6018,7 @@ Flotr.addPlugin('hit', {
         radius = (Math.min(this.canvasWidth, this.canvasHeight) * s.pie.sizeRatio) / 2,
         bisection = n.sAngle<n.eAngle ? (n.sAngle + n.eAngle) / 2: (n.sAngle + n.eAngle + 2* Math.PI) / 2;
       
-      pos += 'bottom:' + (m - top - center.y - Math.sin(bisection) * radius/2 + this.canvasHeight) + 'px;top:auto;';
+      pos += 'bottom:' + (m - top - center.y - Math.sin(bisection) * radius / 2 + this.canvasHeight - (_globalOptions.OffsetY || 0)) + 'px;top:auto;';
       pos += 'left:' + (m + left + center.x + Math.cos(bisection) * radius/2) + 'px;right:auto;';
 
     // Default
