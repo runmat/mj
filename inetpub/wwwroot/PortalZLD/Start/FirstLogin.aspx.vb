8593fea490c37a85366bc5a36a7cbbf36634ebc5
Marco Carl <marco.carl@kroschke.de>
2016-02-29 11:39:28 +0100

    ZLD: altes Logging ausgebaut

--- inetpub/wwwroot/PortalZLD/Start/FirstLogin.aspx.vb
+++ inetpub/wwwroot/PortalZLD/Start/FirstLogin.aspx.vb
@@ -1,8 +1,8 @@
 ﻿Imports CKG.Base.Kernel
-Imports CKG.Base.Kernel.Common.Common
 
 Partial Public Class FirstLogin
     Inherits System.Web.UI.Page
+
     Private m_User As Security.User
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
@@ -44,7 +44,6 @@ Partial Public Class FirstLogin
                     .lblMessage.Text = ""
                 End With
                 m_User.Login(m_User.UserName, txtNewPwd.Text, Session.SessionID.ToString)
-                Log(m_User.UserID.ToString, "Eigenes Kennwort ändern")
 
                 If m_User.Customer.ForcePasswordQuestion = True Then
                     StandardLogin.Visible = False
@@ -66,54 +65,8 @@ Partial Public Class FirstLogin
             End If
         Catch ex As Exception
             Me.lblError.Text = ex.Message
-            Log(m_User.UserID.ToString, Me.lblError.Text, "ERR")
         End Try
     End Sub
-
-    Private Sub Log(ByVal strIdentification As String, ByVal strDescription As String, Optional ByVal strCategory As String = "APP")
-        Dim logApp As New Logging.Trace(m_User.App.Connectionstring, m_User.App.SaveLogAccessSAP, m_User.App.LogLevel)
-        Dim strUserName As String = m_User.UserName ' strUserName
-        Dim strSessionID As String = Session.SessionID ' strSessionID
-        Dim intSource As Integer = CInt(Request.QueryString("AppID")) ' intSource 
-        Dim strTask As String = "Admin - Kennwortänderung" ' strTask
-        Dim strCustomerName As String = m_User.CustomerName ' strCustomername
-        Dim blnIsTestUser As Boolean = m_User.IsTestUser ' blnIsTestUser
-        Dim intSeverity As Integer = 0 ' intSeverity 
-        Dim tblParameters As DataTable = GetLogParameters() ' tblParameters
-
-        logApp.WriteEntry(strCategory, strUserName, strSessionID, intSource, strTask, strIdentification, strDescription, strCustomerName, m_User.Customer.CustomerId, blnIsTestUser, intSeverity, tblParameters)
-    End Sub
-    Private Function GetLogParameters() As DataTable
-        Try
-            Dim tblPar As New DataTable()
-            With tblPar
-                .Columns.Add("neues Kennwort", System.Type.GetType("System.String"))
-                .Columns.Add("Kennwortbestätigung", System.Type.GetType("System.String"))
-                .Rows.Add(.NewRow)
-                Dim strPw As String = ""
-                Dim intCount As Integer
-                For intCount = 1 To txtNewPwd.Text.Length
-                    strPw &= "*"
-                Next
-                .Rows(0)("neues Kennwort") = strPw
-                Dim strPw2 As String = ""
-                For intCount = 1 To txtNewPwdConfirm.Text.Length
-                    strPw2 &= "*"
-                Next
-            End With
-            Return tblPar
-        Catch ex As Exception
-            Dim dt As New DataTable()
-            dt.Columns.Add("Fehler beim erstellen der Log-Parameter", System.Type.GetType("System.String"))
-            dt.Rows.Add(dt.NewRow)
-            Dim str As String = ex.Message
-            If Not ex.InnerException Is Nothing Then
-                str &= ": " & ex.InnerException.Message
-            End If
-            dt.Rows(0)("Fehler beim erstellen der Log-Parameter") = str
-            Return dt
-        End Try
-    End Function
 
     Protected Sub cmdSetzeFrageAntwort_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdSetzeFrageAntwort.Click
         Try
@@ -125,7 +78,7 @@ Partial Public Class FirstLogin
             End If
         Catch ex As Exception
             Me.lblError.Text = ex.Message
-            Log(m_User.UserID.ToString, Me.lblError.Text, "ERR")
         End Try
     End Sub
+
 End Class
