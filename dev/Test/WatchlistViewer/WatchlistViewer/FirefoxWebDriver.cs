367641dbc0edf769ecfb25a5ea060e03f761e68a
Matthias Jenzen <matthias.jenzen@kroschke.de>
2016-03-29 22:31:48 +0200

    AHW590

--- dev/Test/WatchlistViewer/WatchlistViewer/FirefoxWebDriver.cs
+++ dev/Test/WatchlistViewer/WatchlistViewer/FirefoxWebDriver.cs
@@ -99,7 +99,7 @@ namespace WatchlistViewer
                 var ffBinary = new FirefoxBinary(@"c:\Program Files (x86)\Mozilla Firefox\firefox.exe");
                 var firefoxProfile =
                     new FirefoxProfile(
-                        @"C:\Users\JenzenM\AppData\Roaming\Mozilla\Firefox\Profiles\q9bqzwdx.default");
+                        @"C:\Users\JenzenM\AppData\Roaming\Mozilla\Firefox\Profiles\o58xywpm.default");
                 _driver = new FirefoxDriver(ffBinary, firefoxProfile);
             }
             catch (Exception)
