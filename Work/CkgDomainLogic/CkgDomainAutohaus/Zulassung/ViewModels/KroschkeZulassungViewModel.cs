6cde993d21e61269f3948b59b284b67dde82b75c
Matthias Jenzen <matthias.jenzen@kroschke.de>
2016-03-29 11:48:22 +0200

    new "Kroschke Kennzeichen" graphics

--- Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/ViewModels/KroschkeZulassungViewModel.cs
+++ Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/ViewModels/KroschkeZulassungViewModel.cs
@@ -226,6 +226,10 @@ namespace CkgDomainLogic.Autohaus.ViewModels
                 Zulassung.BankAdressdaten.Bankdaten.Zahlungsart = (Zulassung.BankAdressdaten.CpdMitEinzugsermaechtigung ? "E" : "");
 
             SkipBankAdressdaten = (!Zulassung.BankAdressdaten.Cpdkunde);
+
+            // ToDo: Revert StepsKroschkeZulassung.XML
+            // ToDo: Remove this TEST code
+            SetHalterAdresse(HalterAdressen.First(h => h.Name1.NotNullOrEmpty().ToLower().Contains("express")));
         }
 
         #endregion
