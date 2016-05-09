4335870dfff7bde4e57158cb41828ec48fafd262
Matthias Jenzen <matthias.jenzen@kroschke.de>
2016-05-09 10:14:57 +0200

    SZ Homepage, first draft

---
+++ Work/CkgDomainLogic/CkgDomainAutohaus/Zulassung/Models/UIModels/ZulassungHomepageItem.cs
@@ -0,0 +1,11 @@
+﻿namespace CkgDomainLogic.Autohaus.Models
+{
+    public class ZulassungHomepageItem
+    {
+        public string LocalizeKey { get; set; }
+
+        public string CssClass { get; set; }
+
+        public string AppUrl { get; set; }
+    }
+}
