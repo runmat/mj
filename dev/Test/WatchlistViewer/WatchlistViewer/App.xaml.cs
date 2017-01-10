50a19f61e84ec21a5b84956b6e21ac538e22b86e
Matthias Jenzen <matthias.jenzen@kroschke.de>
2017-01-10 11:05:17 +0100

    WatchlistViewer minors

--- dev/Test/WatchlistViewer/WatchlistViewer/App.xaml.cs
+++ dev/Test/WatchlistViewer/WatchlistViewer/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Management;
+using System.Windows;
 using System.Windows.Markup;
 
 namespace WatchlistViewer
@@ -7,11 +9,33 @@ namespace WatchlistViewer
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            TestNetwork(false);
+            return;
+
             FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage("de-DE"))); 
             
             ProcessHelper.KillAllProcessesOf("WatchlistViewer");
 
             base.OnStartup(e);
+        }
+
+        void TestNetwork(bool enableWlan)
+        {
+            var wmiQuery = new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionId != NULL");
+            var searchProcedure = new ManagementObjectSearcher(wmiQuery);
+            foreach (var item in searchProcedure.Get().Cast<ManagementObject>().Where(item => new [] {"Ethernet", "WLAN"}.Contains((string)item["NetConnectionId"])))
+            {
+                var ps = item.Properties;
+
+                var id = item["NetConnectionId"].ToString();
+                var availability = (bool)item["NetEnabled"];
+
+                if (id == "WLAN")
+                    item.InvokeMethod(enableWlan ? "Enable" : "Disable", null);
+
+                if (id == "Ethernet")
+                    item.InvokeMethod(enableWlan ? "Disable" : "Enable", null);
+            }
         }
     }
 }
