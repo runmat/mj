376820b5c5f7dd42eb7bd905ae4030371c5fc4a3
Matthias Jenzen <matthias.jenzen@kroschke.de>
2017-04-05 13:54:55 +0200

    new console app "tear.exe" for executing web requests

---
+++ dev/work/Tools/Tear/Tear/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Net;
+// ReSharper disable UnusedMethodReturnValue.Local
+
+namespace Tear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            GetWebRequestHtml(args[0]);
+        }
+
+        private static string GetWebRequestHtml(string url)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(url);
+            var html = "";
+
+            using (var response = (HttpWebResponse) request.GetResponse())
+            using (var stream = response.GetResponseStream())
+                if (stream != null)
+                    using (var reader = new StreamReader(stream))
+                    {
+                        html = reader.ReadToEnd();
+                    }
+
+            return html;
+        }
+    }
+}
