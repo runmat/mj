51438611d8cdbbc2127beee8133e0929cdfc0c6c
Matthias Jenzen <matthias.jenzen@kroschke.de>
2017-05-10 16:55:39 +0200

    gps import, further work

--- dev/Test/PdfPrint/PdfPrint/App.xaml.cs
+++ dev/Test/PdfPrint/PdfPrint/App.xaml.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using GeneralTools.Models;
@@ -20,16 +21,61 @@ namespace PdfPrint
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            GpsLogImport();
+            var list = GpsLogImport(@"C:\Backup\GpsLog_t.dat");
+            GpxLogExport(list.Where(g => g.DateTime.Date == new DateTime(2017, 05, 10).Date), @"C:\Backup\Matz_t.gpx");
 
             Process.GetCurrentProcess().Kill();
-                    
             base.OnStartup(e);
         }
 
-        static void GpsLogImport()
+        static void GpxLogExport(IEnumerable<ActivityItem> list, string fileName)
         {
-            const string fileName = @"C:\Backup\GpsLog.dat";
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+
+//            var item = list.ToArray()[336];
+//            var gpsLocation = $"{FormatGpsDouble(item.Latitude)},{FormatGpsDouble(item.Longditude)}";
+//            Clipboard.SetText(gpsLocation);
+
+            var f = File.CreateText(fileName);
+            f.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>");
+            f.WriteLine("<gpx version=\"1.1\" creator=\"Matzi\">");
+
+            f.WriteLine("<trk>");
+            f.WriteLine("<name>Matzis Aktivität</name>");
+            f.WriteLine("<desc>(Diese Aktivität wurde aufgezeichnet mit Matzis GpsLog App)</desc>");
+            f.WriteLine("<trkseg>");
+
+            foreach (var activityItem in list)
+            {
+                f.WriteLine($"<trkpt lat=\"{FormatGpsDouble(activityItem.Latitude)}\" lon=\"{FormatGpsDouble(activityItem.Longditude)}\">");
+                f.WriteLine("<ele>0.0</ele>");
+                f.WriteLine($"<time>{FormatGpsDateTime(activityItem.DateTime)}</time>");
+                f.WriteLine("</trkpt>");
+            }
+
+            f.WriteLine("</trkseg>");
+            f.WriteLine("</trk>");
+
+            f.WriteLine("</gpx>");
+
+            f.Close();
+        }
+
+        static string FormatGpsDouble(double val)
+        {
+            return val.ToString().Replace(",", ".");
+        }
+
+        static string FormatGpsDateTime(DateTime val)
+        {
+            return val.ToString("yyyy-MM-dd.THH:mm:ssZ");
+        }
+
+        static IEnumerable<ActivityItem> GpsLogImport(string fileName)
+        {
+            var list = new List<ActivityItem>();
+
             const int dataItemLength = 25;
 
             var f = new FileStream(fileName, FileMode.Open);
@@ -38,22 +84,32 @@ namespace PdfPrint
             var dataItem = br.ReadBytes(dataItemLength);
             while (dataItem.Length == dataItemLength)
             {
-                ProcessDataItem(dataItem);
+                list.Add(ProcessDataItem(dataItem));
 
                 dataItem = br.ReadBytes(dataItemLength);
             }
 
             br.Close();
             f.Close();
+
+            return list;
         }
 
-        static void ProcessDataItem(byte[] dataItem)
+        static ActivityItem ProcessDataItem(byte[] dataItem)
         {
             var dateTime = JavaDateTimeBytesToDateTime(dataItem, 0);
             var activityType = dataItem[8];
 
-            var locationLatitudeBin = JavaDoubleBytesToDouble(dataItem, 9);
-            var locationLongditudeBin = JavaDoubleBytesToDouble(dataItem, 17);
+            var locationLatitude = JavaDoubleBytesToDouble(dataItem, 9);
+            var locationLongditude = JavaDoubleBytesToDouble(dataItem, 17);
+
+            return new ActivityItem
+            {
+                DateTime = dateTime,
+                ActiviyType = activityType,
+                Latitude = locationLatitude,
+                Longditude = locationLongditude
+            };
         }
 
 
@@ -62,7 +118,7 @@ namespace PdfPrint
         private static readonly Int64Converter Int64Converter = new Int64Converter();
 
         [StructLayout(LayoutKind.Explicit)]
-        private struct Double2ulong
+        private struct Double2Ulong
         {
             [FieldOffset(0)] public double d;
             [FieldOffset(0)] public ulong ul;
@@ -99,7 +155,7 @@ namespace PdfPrint
             {
                 var hexString = "" + BitConverter.ToString(bytes, startIndex).SubstringTry(0, 23).Replace("-", "");
 
-                var d2Ul = new Double2ulong();
+                var d2Ul = new Double2Ulong();
                 var parsed = ulong.Parse(hexString, NumberStyles.AllowHexSpecifier);
                 d2Ul.ul = parsed;
                 var dbl = d2Ul.d;
---
+++ dev/work/Tools/GeneralTools/Models/ActivityItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GeneralTools.Models
+{
+    public class ActivityItem
+    {
+        public DateTime DateTime { get; set; }
+
+        public int ActiviyType { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longditude { get; set; }
+    }
+}
