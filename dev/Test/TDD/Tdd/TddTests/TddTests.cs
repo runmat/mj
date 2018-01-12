35acffc6d2b8c7599501153f07295710925db636
Matthias Jenzen <matthias.jenzen@kroschke.de>
2018-01-12 13:39:53 +0100

    Tdd, first draft

---
+++ dev/Test/TDD/Tdd/TddTests/TddTests.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable RedundantUsingDirective
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddTests
+{
+    public class TddTests
+    {
+    }
+}
