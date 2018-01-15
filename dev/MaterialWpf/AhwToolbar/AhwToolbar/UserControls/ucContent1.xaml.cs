23607abb7727e5502c7dddd0defd259fadcbc042
Matthias Jenzen <matthias.jenzen@kroschke.de>
2018-01-15 15:45:50 +0100

    ...

--- dev/MaterialWpf/AhwToolbar/AhwToolbar/UserControls/ucContent1.xaml.cs
+++ dev/MaterialWpf/AhwToolbar/AhwToolbar/UserControls/ucContent1.xaml.cs
@@ -1,8 +1,5 @@
 ﻿namespace AhwToolbar.UserControls
 {
-    /// <summary>
-    /// Interaktionslogik für ucContent1.xaml
-    /// </summary>
     public partial class UcContent1 
     {
         public UcContent1()
--- dev/MaterialWpf/AhwToolbar/AhwToolbar/UserControls/ucContent2.xaml.cs
+++ dev/MaterialWpf/AhwToolbar/AhwToolbar/UserControls/ucContent2.xaml.cs
@@ -1,8 +1,5 @@
 ﻿namespace AhwToolbar.UserControls
 {
-    /// <summary>
-    /// Interaktionslogik für ucContent2.xaml
-    /// </summary>
     public partial class UcContent2 
     {
         public UcContent2()
