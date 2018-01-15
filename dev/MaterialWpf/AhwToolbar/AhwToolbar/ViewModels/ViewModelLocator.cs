a35347f10a7bde3a674c45b6ea7e6bae9cab2c17
Matthias Jenzen <matthias.jenzen@kroschke.de>
2018-01-15 15:42:45 +0100

    minors

--- dev/MaterialWpf/AhwToolbar/AhwToolbar/ViewModels/ViewModelLocator.cs
+++ dev/MaterialWpf/AhwToolbar/AhwToolbar/ViewModels/ViewModelLocator.cs
@@ -12,20 +12,17 @@ namespace AhwToolbar.ViewModels
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
+            // ViewModels
             SimpleIoc.Default.Register<MainViewModel>();
             SimpleIoc.Default.Register<Content1ViewModel>();
             SimpleIoc.Default.Register<Content2ViewModel>();
 
+            // Services
             SimpleIoc.Default.Register<IDataService, DataServiceSql>();
         }
 
         public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
         public Content1ViewModel Content1ViewModel => ServiceLocator.Current.GetInstance<Content1ViewModel>();
         public Content2ViewModel Content2ViewModel => ServiceLocator.Current.GetInstance<Content2ViewModel>();
-
-        public static void Cleanup()
-        {
-            // TODO Clear the ViewModels
-        }
     }
 }
