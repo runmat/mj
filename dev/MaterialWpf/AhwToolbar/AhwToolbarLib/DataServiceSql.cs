d2158206f55db79d879c53cb769bb91397e3dfe0
Matthias Jenzen <matthias.jenzen@kroschke.de>
2018-01-15 14:03:16 +0100

    AhwToolbarLib, created class libary

---
+++ dev/MaterialWpf/AhwToolbar/AhwToolbarLib/DataServiceSql.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable RedundantUsingDirective
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AhwToolbarLib
+{
+    public class DataServiceSql : IDataService
+    {
+    }
+}
---
+++ dev/MaterialWpf/AhwToolbar/AhwToolbarLib/IDataService.cs
@@ -0,0 +1,6 @@
+﻿namespace AhwToolbarLib
+{
+    public interface IDataService
+    {
+    }
+}
