d18e48a9a66d446c6052ceaf07d232fe9a1fa9b8
Matthias Jenzen <matthias.jenzen@kroschke.de>
2018-01-17 15:43:30 +0100

    xaml code restructuring

---
+++ dev/MaterialWpf/AhwToolbar/AhwToolbar/UserControls/ucHeaderMenu.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AhwToolbar.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für ucHeaderMenu.xaml
+    /// </summary>
+    public partial class UcHeaderMenu : UserControl
+    {
+        public UcHeaderMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+    }
+}
