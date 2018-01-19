e9abf0aaa51949644a29e936af6df15fb0300a04
Matthias Jenzen <matthias.jenzen@kroschke.de>
2018-01-19 11:15:53 +0100

    TDD on AhwToolbar, first draft

--- dev/MaterialWpf/AhwToolbar/AhwToolbarLib/Models/ViewModelData.cs
+++ dev/MaterialWpf/AhwToolbar/AhwToolbarLib/Models/ViewModelData.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using GeneralTools.Models;
 
 namespace AhwToolbar.Models
 {
@@ -6,10 +10,31 @@ namespace AhwToolbar.Models
     {
         private static readonly List<TabData> DefaultTabs = new List<TabData>
         {
-            new TabData { Header = "Walter", UserControlType = "AhwToolbar.UserControls.UcContent1", IsSelected = false},
-            new TabData { Header = "Zabel", UserControlType = "AhwToolbar.UserControls.UcContent2", IsSelected = true},
+            new TabData {Header = "Walter", UserControlType = "AhwToolbar.UserControls.UcContent1", IsSelected = false},
+            new TabData {Header = "Zabel", UserControlType = "AhwToolbar.UserControls.UcContent2", IsSelected = true},
         };
 
         public List<TabData> Tabs { get; set; } = DefaultTabs;
+
+
+        public string PersistViewModelDataGetFilename()
+        {
+            var persistFileName = ConfigurationManager.AppSettings["ViewModelDataFileLocation"];
+            if (persistFileName.IsNullOrEmpty())
+                throw new Exception("appSettings Key 'ViewModelDataFileLocation'does not exist in App.Config");
+
+            if (!persistFileName.Contains("[LocalApplicationData]"))
+                throw new Exception("appSettings Value 'ViewModelDataFileLocation' must contain a [LocalApplicationData] string part");
+
+            return persistFileName.Replace("[LocalApplicationData]", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+        }
+
+        public void PersistViewModelData()
+        {
+            var fileName = PersistViewModelDataGetFilename();
+
+            if (!File.Exists(fileName))
+                File.CreateText(fileName);
+        }
     }
 }
---
+++ dev/MaterialWpf/AhwToolbar/AhwToolbarTests/ViewModelDataTests.cs
@@ -0,0 +1,51 @@
+﻿// ReSharper disable RedundantUsingDirective
+using System;
+using System.IO;
+using System.Linq;
+using AhwToolbar.Models;
+using GeneralTools.Models;
+using GeneralTools.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AhwToolbarTests
+{
+    [TestClass]
+    public class ViewModelDataTests
+    {
+        private ViewModelData _viewModel;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _viewModel = new ViewModelData();
+        }
+
+        [TestMethod]
+        public void TestPersistViewModelData()
+        {
+            var fileName =_viewModel.PersistViewModelDataGetFilename();
+            Assert.IsTrue(fileName.IsNotNullOrEmpty());
+
+            if (!FileService.PathExistsAndWriteEnabled(fileName))
+                Assert.Fail("File path 'ViewModelDataFileLocation' is not write enabled!");
+
+            var savedFileName = "";
+            if (File.Exists(fileName))
+            {
+                savedFileName = Path.GetFileNameWithoutExtension(fileName) + "-tmp." + Path.GetExtension(fileName);
+                File.Copy(fileName, savedFileName);
+                File.Delete(fileName);
+            }
+
+            _viewModel.PersistViewModelData();
+
+            if (!File.Exists(fileName))
+            {
+                if (savedFileName.IsNotNullOrEmpty())
+                    File.Copy(savedFileName, fileName);
+
+                Assert.Fail("File 'ViewModelDataFileLocation' was not created!");
+            }
+        }
+    }
+}
